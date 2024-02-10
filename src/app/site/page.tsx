0bb9c6eb51b954ab0db85513242273db66d47b5f
OussamaDeghfel <oussama.deghfel@univ-constantine2.dz>
2024-02-10 16:51:31 +0100

    some changes

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,13 +1,19 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { pricingCards } from "@/lib/constants";
+import clsx from "clsx";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col bg-black">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] " />
-        <p className="text-white z-10 text-center">
-          run your agency in one place
-        </p>
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-[10]" />
+        <p className="text-center">Run your agency in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">
             Plura
@@ -22,6 +28,34 @@ export default function Home() {
             className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
           />
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 md:mt-20">
+        <h2 className="text-4xl text-center">Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center">
+          {" "}
+          Our Straightforward pricing plans are tailored to meet your needs. if{" "}
+          {" you're "}
+          <br /> not ready to commit you can get started for free.
+        </p>
+        <div className="flex items-center justify-center gap-4 flex-wrap mt-6">
+          {pricingCards.map((card) => (
+            <Card
+              key={card.title}
+              className={clsx("w-[300px] flex flex-col justify-between", {
+                "border-2 border-primary": card.title === "Unlimited Saas",
+              })}
+            >
+              <CardHeader
+                className={clsx("", {
+                  "text-muted-foreground": card.title !== "Unlimited Saas",
+                })}
+              >
+                <CardTitle> {card.title} </CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+              </CardHeader>
+            </Card>
+          ))}
         </div>
       </section>
     </>
