ed08d76a6c00c08a8533bf102301faf92e11de9e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-21 14:49:26 +0100

    add <Unauthorized /> component

---
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -0,0 +1,32 @@
+import Unauthorized from '@/components/unauthorized'
+import { verifyAndAcceptInvitation } from '@/lib/queries'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+    params: {agencyId: string}
+}
+
+const layout = async ({children, params}: Props) => {
+    const agencyId = await verifyAndAcceptInvitation()
+    const user = await currentUser()
+    
+    if(!user){
+        return redirect('/')
+    }
+
+    if(!agencyId){
+        return redirect('/agecny')
+    }
+
+    if(
+        user.privateMetadata.role !== 'AGENCY_OWNER' &&
+        user.privateMetadata.role !== 'AGENCY_ADMIN' 
+    )
+    return <Unauthorized />
+    
+}
+
+export default layout
