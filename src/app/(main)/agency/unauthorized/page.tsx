7a8c679f580627f24e400a60b080462467ff5faa
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-21 14:48:38 +0100

    create this in agecny folder for the routing

---
+++ src/app/(main)/agency/unauthorized/page.tsx
@@ -0,0 +1,12 @@
+import Unauthorized from '@/components/unauthorized'
+import React from 'react'
+
+type Props = {}
+
+function Page({}: Props) {
+  return (
+    <Unauthorized />
+  )
+}
+
+export default Page
