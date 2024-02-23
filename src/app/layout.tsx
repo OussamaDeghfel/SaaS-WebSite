2289f67d2eb669ffd87252099098079a09da6f57
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-23 11:40:41 +0100

    implying {Children} inside ModalProvider Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { dark } from "@clerk/themes";
+import ModalProvider from "@/providers/modal-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const font = DM_Sans({ subsets: ["latin"] });
 
@@ -26,7 +28,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <ModalProvider>
+            {children}
+            <Toaster />      
+          </ModalProvider>
         </ThemeProvider>
       </body>
     </html>
