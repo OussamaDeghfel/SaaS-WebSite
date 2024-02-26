a90b93abed326e635ef0263e9adb5f100c3fb67a
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-26 13:15:02 +0100

    add component for create sub account btn

---
+++ src/app/(main)/agency/[agencyId]/all-subaccounts/_components/create-subaccount-btn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CreateSubaccountBtn = (props: Props) => {
+  return (
+    <div>CreateSubaccountBtn</div>
+  )
+}
+
+export default CreateSubaccountBtn
