024c0cb54889173f4b3091489bb862f2151f27ab
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-26 14:41:12 +0100

    team component implement types and some fetch of data

--- src/app/(main)/agency/[agencyId]/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -1,8 +1,25 @@
+import { db } from '@/lib/db'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  params: {agencyId: string}
+}
 
-const Team = (props: Props) => {
+const Team = async ({params}: Props) => {
+  const authUser = await db.user.findMany({
+    where: {
+      Agency: {
+        id: params.agencyId,
+      }
+    },
+    include: {
+      Agency: { include :{ SubAccount: true}},
+      Permissions: {include : {SubAccount: true}}
+    }
+  })
+
+  if(!authUser) return
+
   return (
     <div>Team</div>
   )
