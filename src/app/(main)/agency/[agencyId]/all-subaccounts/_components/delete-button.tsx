944b845713286855865a50210cd4aaf4e66ac200
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-26 12:56:15 +0100

    add saveActivityLogs

--- src/app/(main)/agency/[agencyId]/all-subaccounts/_components/delete-button.tsx
+++ src/app/(main)/agency/[agencyId]/all-subaccounts/_components/delete-button.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { getSubaccountDetails } from '@/lib/queries'
+import { deleteSubAccount, getSubaccountDetails, saveActivityLogsNotification } from '@/lib/queries'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
@@ -14,7 +14,16 @@ const DeleteButton = ({subaccountId}: Props) => {
   return (
     <div onClick={async() => {
         const response = await getSubaccountDetails(subaccountId)
-    }}></div>
+        await saveActivityLogsNotification({
+            agencyId: undefined,
+            description: `Deleted a subaccount | ${response?.name}`,
+            subaccountId,
+          })
+          await deleteSubAccount(subaccountId)
+          router.refresh()
+    }}>
+        Delete Sub Account
+    </div>
   )
 }
 
