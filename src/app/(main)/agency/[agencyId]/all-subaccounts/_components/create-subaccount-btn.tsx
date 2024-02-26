f0ce7a6b779c386a24a671436a2f63d94fe0d142
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-26 13:55:31 +0100

    add custommodal inside onClick button

--- src/app/(main)/agency/[agencyId]/all-subaccounts/_components/create-subaccount-btn.tsx
+++ src/app/(main)/agency/[agencyId]/all-subaccounts/_components/create-subaccount-btn.tsx
@@ -1,6 +1,10 @@
+import SubAccountDetails from '@/components/forms/subaccount-details'
+import CustomModal from '@/components/global/custom-modal'
+import { Button } from '@/components/ui/button'
 import { useModal } from '@/providers/modal-provider'
 import { Agency, AgencySidebarOption, SubAccount, User } from '@prisma/client'
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type Props = {
     user: User & {
@@ -24,7 +28,23 @@ const CreateSubaccountBtn = ({className, id, user}: Props) => {
 
     if(!agencyDetails) return
   return (
-    <div>CreateSubaccountBtn</div>
+    <Button
+        className={twMerge('w-full flex gap-4', className)}
+        onClick={() => {
+            setOpen(
+                <CustomModal 
+                    title='Create a Subaccount'
+                    subheading='You can switch between'
+                >
+                    <SubAccountDetails 
+                        agencyDetails={agencyDetails}
+                        userId={user.id}
+                        userName={user.name}
+                    />
+                </CustomModal>
+            )
+        }}
+    ></Button>
   )
 }
 
