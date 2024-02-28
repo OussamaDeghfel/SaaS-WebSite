46bf0e7c2ce6d04345a472ab22615d1d8e3a982e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-02-28 16:34:45 +0100

    create launchpad subaccount component

---
+++ src/app/(main)/subaccount/[subaccountId]/launchpad/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const LaunchPadSubaccountPage = (props: Props) => {
+  return (
+    <div>LaunchPadSubaccountPage</div>
+  )
+}
+
+export default LaunchPadSubaccountPage
