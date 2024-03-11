1604d4d774b1a30c79fd017fadbc697cd2676c80
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-11 12:18:35 +0100

    updatename and add type

---
+++ src/components/forms/upload-media.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+  subaccountId:string
+}
+
+const UploadMediaForm = ({subaccountId}: Props) => {
+  return (
+    <div>UploadMediaForm</div>
+  )
+}
+
+export default UploadMediaForm
