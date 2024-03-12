11a8dfd20cdc9483731980953b15ce0c27030846
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-12 17:48:21 +0100

    change style

--- src/components/media/index.tsx
+++ src/components/media/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 const MediaComponent = ({data, subaccountId}: Props) => {
   return (
     <div className='h-full w-full flex gap-4 flex-col'>
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-between items-center'>
             <h1 className='text-4xl'>Media Bucket</h1>
             <MediaUploadbutton subaccountId={subaccountId} />
         </div>
