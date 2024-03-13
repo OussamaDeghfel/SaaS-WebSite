99c276b008f362ec0bfafc0dd56cd45dbd148724
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-13 14:51:43 +0100

    pipelinesDetails gets and add lanes is related to pipelines

--- src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelinesId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelinesId]/page.tsx
@@ -1,10 +1,31 @@
+import { Tabs } from '@/components/ui/tabs';
+import { db } from '@/lib/db';
+import { getLanesWithTicketAndTags, getPipelineDetails } from '@/lib/queries';
+import { LaneDetail } from '@/lib/types';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-type Props = {}
+type Props = {
+    params: {subaccountId: string; pipelineId: string }
+}
 
-const PipelinePage = (props: Props) => {
+const PipelinePage = async({params}: Props) => {
+    const pipelineDetails = await getPipelineDetails(params.pipelineId)
+
+    if(!pipelineDetails)
+        return redirect(`/subaccount/${params.subaccountId}/pipelines`)
+
+    const pipelins = await db.pipeline.findMany({
+        where: {subAccountId: params.subaccountId}
+    })    
+
+    const lanes = (await getLanesWithTicketAndTags(params.pipelineId)) as LaneDetail[]
+
   return (
-    <div>PipelinePage</div>
+    <Tabs 
+        defaultValue='view'
+        className='w-full'
+    ></Tabs>
   )
 }
 
