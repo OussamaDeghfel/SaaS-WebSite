ff0932443f67745b554fb282f8b15137d36d7655
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-14 10:21:54 +0100

    rename a Prop

--- src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-infobat.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-infobat.tsx
@@ -4,12 +4,12 @@ import { Pipeline } from '@prisma/client'
 import React from 'react'
 
 type Props = {
-    subaccountId : string
+    subAccountId : string
     pipelines: Pipeline[]
     pipelineId: string
 }
 
-const PipelineInfobar = ({subaccountId, pipelineId ,pipelines}: Props) => {
+const PipelineInfobar = ({subAccountId, pipelineId ,pipelines}: Props) => {
     const { setOpen: setOpenModal, setClose } = useModal()
     const [open, setOpen] = React.useState(false)
     const [value, setValue] = React.useState(pipelineId)
