468a1e9c9d9c9bbd771c40a78444e30cf0f17925
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-16 14:46:51 +0100

    add button of creation the new lane

--- src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
@@ -1,7 +1,9 @@
 'use client'
+import { Button } from '@/components/ui/button'
 import { LaneDetail, PipelineDetailsWithLanesCardsTagsTickets } from '@/lib/types'
 import { useModal } from '@/providers/modal-provider'
 import { Lane, Ticket } from '@prisma/client'
+import { Plus } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import {DragDropContext , DropResult, Droppable} from "react-beautiful-dnd"
@@ -36,6 +38,13 @@ const PipelineView = ({
         <div className='bg-white/60 dark:bg-background/60 rounded-xl p-4 use-automation-zoom-in'>
             <div className='flex items-center justify-between'>
                 <h1 className='text-2xl'>{pipelineDetails?.name}</h1>
+                <Button
+                    className='flex items-center gap-4'
+                    onClick={handleAddlane}
+                >
+                    <Plus size={15} />
+                    Create Lane
+                </Button>
             </div>
         </div>
     </DragDropContext>
