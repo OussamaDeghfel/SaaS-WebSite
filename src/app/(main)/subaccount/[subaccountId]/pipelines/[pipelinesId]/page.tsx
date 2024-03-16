a5f8564d76066f257537b3c15f47db3d3c8f52bd
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-16 14:16:19 +0100

    edit infobar important , add pipelineview component with their attributes

--- src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelinesId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelinesId]/page.tsx
@@ -1,11 +1,12 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { db } from '@/lib/db';
-import { getLanesWithTicketAndTags, getPipelineDetails } from '@/lib/queries';
+import { getLanesWithTicketAndTags, getPipelineDetails, updateLanesOrder, updateTicketsOrder } from '@/lib/queries';
 import { LaneDetail } from '@/lib/types';
 import { redirect } from 'next/navigation';
 import React from 'react'
-import PipelineInfobar from '../_components/pipeline-infobat';
+import PipelineInfobar from '../_components/pipeline-infobar';
 import PipelineSettings from '../_components/pipeline-settings';
+import PipelineView from '../_components/pipeline-view';
 
 type Props = {
     params: {subaccountId: string; pipelineId: string }
@@ -42,7 +43,16 @@ const PipelinePage = async({params}: Props) => {
                 <TabsTrigger value='Settings'>Setting</TabsTrigger>
              </div>
         </TabsList>
-        <TabsContent value='view'>View</TabsContent>
+        <TabsContent value='view'>
+            <PipelineView 
+                lanes={lanes}
+                pipelineDetails={pipelineDetails}
+                pipelineId={params.pipelineId}
+                subaccountId={params.subaccountId}
+                updateLanesOrder={updateLanesOrder}
+                updateTicketsOrder={updateTicketsOrder}
+            />
+        </TabsContent>
         <TabsContent value='Settings'>
             <PipelineSettings
                 pipelineId={params.pipelineId}
