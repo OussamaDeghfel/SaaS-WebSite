cf446aa8452e456b681614041f5ded3fe6cbc0b9
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-16 14:13:04 +0100

    define it as "use Client" add props

--- src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
@@ -1,6 +1,16 @@
+'use client'
+import { LaneDetail, PipelineDetailsWithLanesCardsTagsTickets } from '@/lib/types'
+import { Lane, Ticket } from '@prisma/client'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    lanes: LaneDetail[]
+    pipelineId: string
+    subaccountId: string
+    pipelineDetails: PipelineDetailsWithLanesCardsTagsTickets
+    updateLanesOrder: (lanes: Lane[]) => Promise<void>
+    updateTicketsOrder: (tickets: Ticket[]) => Promise<void>
+}
 
 const PipelineView = (props: Props) => {
   return (
