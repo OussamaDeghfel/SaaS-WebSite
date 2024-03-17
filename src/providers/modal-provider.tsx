787e2cebe9718a6aa7429523ac793f1b286bc606
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-17 14:05:20 +0100

    add type of ticket

--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { TicketDetails } from '@/lib/types'
 //import { PricesList, TicketDetails } from '@/lib/types'
 import { Agency, Contact, Plan, User } from '@prisma/client'
 import { createContext, useContext, useEffect, useState } from 'react'
@@ -11,7 +12,7 @@ interface ModalProviderProps {
 export type ModalData = {
   user?: User
   agency?: Agency
-  //ticket?: TicketDetails[0]
+  ticket?: TicketDetails[0]
   contact?: Contact
   plans?: {
     defaultPriceId: Plan
