4e96abe4540145e9a303eb621b0d7e0034c554d2
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-17 13:54:15 +0100

    add props

--- src/components/forms/ticket-form.tsx
+++ src/components/forms/ticket-form.tsx
@@ -1,7 +1,12 @@
 'use client'
+import { TicketWithTags } from '@/lib/types'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  laneId: string
+  subaccountId: string
+  getNewTicket: (ticket: TicketWithTags[0]) => void
+}
 
 const TicketForm = (props: Props) => {
   return (
