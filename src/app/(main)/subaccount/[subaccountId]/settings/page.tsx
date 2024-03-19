fc347c2cffe0fbe698794b40eb1d8503ec0a2a7a
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-19 14:00:17 +0100

    await cause it's fetch data

--- src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
@@ -1,22 +1,21 @@
-import { db } from '@/lib/db'
-import { currentUser } from '@clerk/nextjs'
-import React from 'react'
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs";
+import React from "react";
 
 type Props = {
-  params: {subaccountId: string}
-}
+  params: { subaccountId: string };
+};
 
-const SubaccountSettingpage = async ({params}: Props) => {
-  const authUser = await currentUser()
-  if(!authUser) return 
-   const useDetails = db.user.findUnique({
-    where : {
-      email: authUser.emailAddresses[0].emailAddress
-    }
-   })
-  return (
-    <div>SubaccountSettingpage</div>
-  )
-}
+const SubaccountSettingpage = async ({ params }: Props) => {
+  const authUser = await currentUser();
+  if (!authUser) return;
+  const useDetails = await db.user.findUnique({
+    where: {
+      email: authUser.emailAddresses[0].emailAddress,
+    },
+  });
 
-export default SubaccountSettingpage
+  return <div>SubaccountSettingpage</div>;
+};
+
+export default SubaccountSettingpage;
