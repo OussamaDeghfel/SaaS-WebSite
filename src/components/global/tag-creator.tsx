251512cdfe003db51ac73dbe20045262ca43ee67
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-19 11:22:54 +0100

    define types and add hooks

--- src/components/global/tag-creator.tsx
+++ src/components/global/tag-creator.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+"use client";
+import { Tag } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-type Props = {}
+type Props = {
+  subAccountId: string;
+  getSelectedTags: (tags: Tag[]) => void;
+  defaultTags?: Tag[];
+};
 
-const TagCreator = (props: Props) => {
-  return (
-    <div>TagCreator</div>
-  )
-}
+const TagColors = ["BLUE", "ORANGE", "ROSE", "PURPLE", "GREEN"] as const;
+export type TagColor = (typeof TagColors)[number];
 
-export default TagCreator
+const TagCreator = ({ subAccountId, getSelectedTags, defaultTags }: Props) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(defaultTags || []);
+  const [tags, setTags] = useState<Tag[]>([]);
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+
+  useEffect(() => {
+    getSelectedTags(selectedTags);
+  }, [selectedTags]);
+  
+  return <div>TagCreator</div>;
+};
+
+export default TagCreator;
