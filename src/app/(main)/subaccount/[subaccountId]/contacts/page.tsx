9c27212aba33eab772c48e7b83e912187b163b6a
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-19 14:39:52 +0100

    add types

--- src/app/(main)/subaccount/[subaccountId]/contacts/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/contacts/page.tsx
@@ -1,8 +1,35 @@
+import { db } from '@/lib/db'
+import { Contact, SubAccount, Ticket } from '@prisma/client'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  params: {subaccountId: string}
+}
 
-const ContactPage = (props: Props) => {
+const ContactPage = async ({params}: Props) => {
+  type SubAccountWithContacts = SubAccount & {
+    Contact: (Contact & { Ticket: Ticket[] })[]
+  }
+  const contacts = (await db.subAccount.findUnique({
+    where: {
+      id: params.subaccountId,
+    },
+
+    include: {
+      Contact: {
+        include: {
+          Ticket: {
+            select: {
+              value: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      },
+    },
+  })) as SubAccountWithContacts
   return (
     <div>ContactPage</div>
   )
