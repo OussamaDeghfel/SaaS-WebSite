02c9eb8cb85cad465d41a8f523310f57f2406006
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-19 14:45:12 +0100

    add table

--- src/app/(main)/subaccount/[subaccountId]/contacts/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/contacts/page.tsx
@@ -1,6 +1,10 @@
 import BlurPage from '@/components/global/blur-page'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { db } from '@/lib/db'
 import { Contact, SubAccount, Ticket } from '@prisma/client'
+import { format } from 'date-fns'
 import React from 'react'
 
 type Props = {
@@ -52,6 +56,43 @@ const ContactPage = async ({params}: Props) => {
   return (
     <BlurPage>
       <h1 className="text-4xl p-4">Contacts</h1>
+      <Table>
+      <TableHeader>
+          <TableRow>
+            <TableHead className="w-[200px]">Name</TableHead>
+            <TableHead className="w-[300px]">Email</TableHead>
+            <TableHead className="w-[200px]">Active</TableHead>
+            <TableHead>Created Date</TableHead>
+            <TableHead className="text-right">Total Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody className="font-medium truncate">
+          {allContacts.map((contact) => (
+            <TableRow key={contact.id}>
+              <TableCell>
+                <Avatar>
+                  <AvatarImage alt="@shadcn" />
+                  <AvatarFallback className="bg-primary text-white">
+                    {contact.name.slice(0, 2).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              </TableCell>
+              <TableCell>{contact.email}</TableCell>
+              <TableCell>
+                {formatTotal(contact.Ticket) === '$0.00' ? (
+                  <Badge variant={'destructive'}>Inactive</Badge>
+                ) : (
+                  <Badge className="bg-emerald-700">Active</Badge>
+                )}
+              </TableCell>
+              <TableCell>{format(contact.createdAt, 'MM/dd/yyyy')}</TableCell>
+              <TableCell className="text-right">
+                {formatTotal(contact.Ticket)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </BlurPage>
   )
 }
