c9bc7782a26e084352a519afcf18494daa5d4d18
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-19 10:28:15 +0100

    add select of members and button for customer

--- src/components/forms/ticket-form.tsx
+++ src/components/forms/ticket-form.tsx
@@ -1,83 +1,106 @@
-'use client'
-import { getSubAccountTeamMembers, saveActivityLogsNotification, searchContacts, upsertTicket } from '@/lib/queries'
-import { TicketFormSchema, TicketWithTags } from '@/lib/types'
-import { useModal } from '@/providers/modal-provider'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Contact, Tag, User } from '@prisma/client'
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useRef, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { setTimeout } from 'timers'
-import { z } from 'zod'
-import { toast } from '../ui/use-toast'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
-import { Input } from '../ui/input'
-import { Textarea } from '../ui/textarea'
+"use client";
+import {
+  getSubAccountTeamMembers,
+  saveActivityLogsNotification,
+  searchContacts,
+  upsertTicket,
+} from "@/lib/queries";
+import { TicketFormSchema, TicketWithTags } from "@/lib/types";
+import { useModal } from "@/providers/modal-provider";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Contact, Tag, User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { setTimeout } from "timers";
+import { z } from "zod";
+import { toast } from "../ui/use-toast";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { ChevronsUpDownIcon, User2 } from "lucide-react";
+import { Popover, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
 
 type Props = {
-  laneId: string
-  subaccountId: string
-  getNewTicket: (ticket: TicketWithTags[0]) => void
-}
+  laneId: string;
+  subaccountId: string;
+  getNewTicket: (ticket: TicketWithTags[0]) => void;
+};
 
-const TicketForm = ({getNewTicket, subaccountId, laneId}: Props) => {
-  const {data: defaultData, setClose} = useModal()
-  const router = useRouter()
-  const [tags, setTags] = useState<Tag[]>()
-  const [contact, setContact] = useState("")
-  const [search, setSearch] = useState("")
-  const [contactList, setContactList] = useState<Contact[]>([])
-  const saveTimeRef = useRef<ReturnType<typeof setTimeout>>()
-  const [allTeamMembers, setAllTeamMembers] = useState<User[]>([])
+const TicketForm = ({ getNewTicket, subaccountId, laneId }: Props) => {
+  const { data: defaultData, setClose } = useModal();
+  const router = useRouter();
+  const [tags, setTags] = useState<Tag[]>();
+  const [contact, setContact] = useState("");
+  const [search, setSearch] = useState("");
+  const [contactList, setContactList] = useState<Contact[]>([]);
+  const saveTimeRef = useRef<ReturnType<typeof setTimeout>>();
+  const [allTeamMembers, setAllTeamMembers] = useState<User[]>([]);
   const [assignedTo, setAssignedTo] = useState(
-    defaultData.ticket?.Assigned?.id || ''
-  )
+    defaultData.ticket?.Assigned?.id || ""
+  );
   const form = useForm<z.infer<typeof TicketFormSchema>>({
     mode: "onChange",
     resolver: zodResolver(TicketFormSchema),
     defaultValues: {
-      name: defaultData.ticket?.name || '',
-      description: defaultData.ticket?.description || '',
-      value: String(defaultData.ticket?.value || 0)
-    }
-  })
-  const isLoading = form.formState.isLoading
+      name: defaultData.ticket?.name || "",
+      description: defaultData.ticket?.description || "",
+      value: String(defaultData.ticket?.value || 0),
+    },
+  });
+  const isLoading = form.formState.isLoading;
 
-  useEffect(()=> {
-    if(subaccountId){
+  useEffect(() => {
+    if (subaccountId) {
       const fetchData = async () => {
-        const response = await getSubAccountTeamMembers(subaccountId)
-        console.log(response)
-        if(response) setAllTeamMembers(response)
-      }
-    fetchData()
+        const response = await getSubAccountTeamMembers(subaccountId);
+        console.log(response);
+        if (response) setAllTeamMembers(response);
+      };
+      fetchData();
     }
-  },[subaccountId])
+  }, [subaccountId]);
 
-  useEffect(()=> {
-    if(defaultData.ticket){
+  useEffect(() => {
+    if (defaultData.ticket) {
       form.reset({
-        name: defaultData.ticket.name || '',
-        description: defaultData.ticket?.description || '',
-        value: String(defaultData.ticket?.value || 0)
-      })
-      if(defaultData.ticket.customerId)
-        setContact(defaultData.ticket.customerId)
-    
+        name: defaultData.ticket.name || "",
+        description: defaultData.ticket?.description || "",
+        value: String(defaultData.ticket?.value || 0),
+      });
+      if (defaultData.ticket.customerId)
+        setContact(defaultData.ticket.customerId);
+
       const fetchData = async () => {
         const response = await searchContacts(
           //@ts-ignore
           defaultData.ticket?.Customer?.name
-        )
-        setContactList(response)
-      }  
-      fetchData()
+        );
+        setContactList(response);
+      };
+      fetchData();
     }
-  },[defaultData])
+  }, [defaultData]);
 
   const onSubmit = async (values: z.infer<typeof TicketFormSchema>) => {
-    if (!laneId) return
+    if (!laneId) return;
     try {
       const response = await upsertTicket(
         {
@@ -89,29 +112,29 @@ const TicketForm = ({getNewTicket, subaccountId, laneId}: Props) => {
         },
         //@ts-ignore
         tags
-      )
+      );
 
       await saveActivityLogsNotification({
         agencyId: undefined,
         description: `Updated a ticket | ${response?.name}`,
         subaccountId,
-      })
+      });
 
       toast({
-        title: 'Success',
-        description: 'Saved  details',
-      })
-      if (response) getNewTicket(response)
-      router.refresh()
+        title: "Success",
+        description: "Saved  details",
+      });
+      if (response) getNewTicket(response);
+      router.refresh();
     } catch (error) {
       toast({
-        variant: 'destructive',
-        title: 'Oppse!',
-        description: 'Could not save pipeline details',
-      })
+        variant: "destructive",
+        title: "Oppse!",
+        description: "Could not save pipeline details",
+      });
     }
-    setClose()
-  }
+    setClose();
+  };
 
   return (
     <Card>
@@ -122,20 +145,17 @@ const TicketForm = ({getNewTicket, subaccountId, laneId}: Props) => {
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className='flex flex-col gap-4'
+            className="flex flex-col gap-4"
           >
-            <FormField 
+            <FormField
               disabled={isLoading}
               control={form.control}
-              name='name'
-              render={({field}) => (
+              name="name"
+              render={({ field }) => (
                 <FormItem>
                   <FormLabel>Ticket Name</FormLabel>
                   <FormControl>
-                    <Input 
-                      placeholder='Name'
-                      {...field}
-                      />
+                    <Input placeholder="Name" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -149,10 +169,7 @@ const TicketForm = ({getNewTicket, subaccountId, laneId}: Props) => {
                 <FormItem>
                   <FormLabel>Description</FormLabel>
                   <FormControl>
-                    <Textarea
-                      placeholder="Description"
-                      {...field}
-                    />
+                    <Textarea placeholder="Description" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -166,21 +183,72 @@ const TicketForm = ({getNewTicket, subaccountId, laneId}: Props) => {
                 <FormItem>
                   <FormLabel>Ticket Value</FormLabel>
                   <FormControl>
-                    <Input
-                      placeholder="Value"
-                      {...field}
-                    />
+                    <Input placeholder="Value" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
             <h3>Add Tag</h3>
+            <FormLabel>Assigned To Team Member</FormLabel>
+            <Select onValueChange={setAssignedTo} defaultValue={assignedTo}>
+              <SelectTrigger>
+                <SelectValue
+                  placeholder={
+                    <div className="flex items-center gap-2">
+                      <Avatar className="w-8 h-8">
+                        <AvatarImage alt="contact" />
+                        <AvatarFallback className="bg-primary text-sm text-white">
+                          <User2 size={14} />
+                        </AvatarFallback>
+                      </Avatar>
+
+                      <span className="text-sm text-muted-foreground">
+                        Not Assigned
+                      </span>
+                    </div>
+                  }
+                />
+              </SelectTrigger>
+              <SelectContent>
+                {allTeamMembers.map((teamMember) => (
+                  <SelectItem key={teamMember.id} value={teamMember.id}>
+                    <div className="flex items-center gap-2">
+                      <Avatar className="w-8 h-8">
+                        <AvatarImage alt="contact" src={teamMember.avatarUrl} />
+                        <AvatarFallback className="bg-primary text-sm text-white">
+                          <User2 size={14} />
+                        </AvatarFallback>
+                      </Avatar>
+
+                      <span className="text-sm text-muted-foreground">
+                        {teamMember.name}
+                      </span>
+                    </div>
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <FormLabel>Customer</FormLabel>
+            <Popover>
+              <PopoverTrigger asChild className="w-full">
+                <Button 
+                  variant="outline"
+                  role="combobox"
+                  className="justify-between"
+                > 
+                  {contact 
+                  ? contactList.find((c)=> c.id === contact)?.name 
+                  : " Select Customer .... " }
+                  <ChevronsUpDownIcon className="ml-2 h-2 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+            </Popover>
           </form>
         </Form>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default TicketForm
+export default TicketForm;
