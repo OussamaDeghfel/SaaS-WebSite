78c8c46447c7c0771f9e02963770f47cc2b61863
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-20 14:22:17 +0100

    add variable of modalprovider

--- src/app/(main)/agency/[agencyId]/billing/_component/pricing-card.tsx
+++ src/app/(main)/agency/[agencyId]/billing/_component/pricing-card.tsx
@@ -1,25 +1,41 @@
-"use client"
-import { PricesList } from '@/lib/types'
-import React from 'react'
+"use client";
+import { PricesList } from "@/lib/types";
+import { useModal } from "@/providers/modal-provider";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 type Props = {
-    features: string[]
-    buttonCta: string
-    title: string
-    description: string
-    amt: string
-    duration: string
-    highlightTitle: string
-    highlightDescription: string
-    customerId: string
-    prices: PricesList['data']
-    planExists: boolean
-}
+  features: string[];
+  buttonCta: string;
+  title: string;
+  description: string;
+  amt: string;
+  duration: string;
+  highlightTitle: string;
+  highlightDescription: string;
+  customerId: string;
+  prices: PricesList["data"];
+  planExists: boolean;
+};
 
-const PricingCard = (props: Props) => {
-  return (
-    <div>PricingCard</div>
-  )
-}
+const PricingCard = ({
+  amt,
+  buttonCta,
+  customerId,
+  description,
+  duration,
+  features,
+  highlightDescription,
+  highlightTitle,
+  planExists,
+  prices,
+  title,
+}: Props) => {
+    const {setOpen} = useModal()
+    const searchParams = useSearchParams()
+    const plan = searchParams.get('plan')
+    
+  return <div>PricingCard</div>;
+};
 
-export default PricingCard
+export default PricingCard;
