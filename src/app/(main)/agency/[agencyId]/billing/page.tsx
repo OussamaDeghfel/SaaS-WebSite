c47b6e505bff512dd3f1ab32b0596a4d4728f922
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-20 14:30:37 +0100

    add pricingCard with attribute

--- src/app/(main)/agency/[agencyId]/billing/page.tsx
+++ src/app/(main)/agency/[agencyId]/billing/page.tsx
@@ -3,6 +3,7 @@ import { addOnProducts, pricingCards } from "@/lib/constants";
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
 import React from "react";
+import PricingCard from "./_component/pricing-card";
 
 type Props = {
   params: { agencyId: string };
@@ -55,7 +56,45 @@ const Billing = async ({ params }: Props) => {
       <h1 className="text-4xl p-4">Billing</h1>
       <Separator className="mb-6" />
       <h2 className="text-2xl p-2">Current Plan</h2>
-      <div className="flex flex-col lg:!flex-row justify-between gap-8"></div>
+      <div className="flex flex-col lg:!flex-row justify-between gap-8">
+      <PricingCard
+          planExists={agencySubscription?.Subscription?.active === true}
+          prices={prices.data}
+          customerId={agencySubscription?.customerId || ''}
+          amt={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.price || '$0'
+              : '$0'
+          }
+          buttonCta={
+            agencySubscription?.Subscription?.active === true
+              ? 'Change Plan'
+              : 'Get Started'
+          }
+          highlightDescription="Want to modify your plan? You can do this here. If you have
+          further question contact support@plura-app.com"
+          highlightTitle="Plan Options"
+          description={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.description || 'Lets get started'
+              : 'Lets get started! Pick a plan that works best for you.'
+          }
+          duration="/ month"
+          features={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.features || []
+              : currentPlanDetails?.features ||
+                pricingCards.find((pricing) => pricing.title === 'Starter')
+                  ?.features ||
+                []
+          }
+          title={
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.title || 'Starter'
+              : 'Starter'
+          }
+        />
+      </div>
     </>
   );
 };
