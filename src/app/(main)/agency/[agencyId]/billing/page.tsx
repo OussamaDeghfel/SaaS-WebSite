e805170514c391796dade6e64ba6838775e608c3
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-20 13:23:43 +0100

    addOns variable to retrieve list from add-on product

--- src/app/(main)/agency/[agencyId]/billing/page.tsx
+++ src/app/(main)/agency/[agencyId]/billing/page.tsx
@@ -1,8 +1,17 @@
+import { addOnProducts } from '@/lib/constants'
+import { stripe } from '@/lib/stripe'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  params: {agencyId: string}
+}
 
-const Billing = (props: Props) => {
+const Billing = async ({params}: Props) => {
+  const addOns = await stripe.products.list({
+    ids: addOnProducts.map((product) => product.id),
+    expand: ['data.default_price']
+  })
+
   return (
     <div>Billing</div>
   )
