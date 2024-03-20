95ce7bd772a8c0064c7a285336cf2ca2ef28d860
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-20 15:00:47 +0100

    new form

---
+++ src/components/forms/subscription-form/subscription-form-wrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SubscriptionFormWrapper = (props: Props) => {
+  return (
+    <div>SubscriptionFormWrapper</div>
+  )
+}
+
+export default SubscriptionFormWrapper
