d810a1f072da5da650392e4ae80ed44be8f9d7be
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-20 17:40:47 +0100

    add useEffect for Create new subscribe for the customer with new selectedPrice

--- src/components/forms/subscription-form/subscription-form-wrapper.tsx
+++ src/components/forms/subscription-form/subscription-form-wrapper.tsx
@@ -1,39 +1,72 @@
-"use client"
-import { useModal } from '@/providers/modal-provider'
-import { Plan } from '@prisma/client'
-import { StripeElementsOptions } from '@stripe/stripe-js'
-import { useRouter } from 'next/navigation'
-import React, { useMemo, useState } from 'react'
+"use client";
+import { toast } from "@/components/ui/use-toast";
+import { useModal } from "@/providers/modal-provider";
+import { Plan } from "@prisma/client";
+import { StripeElementsOptions } from "@stripe/stripe-js";
+import { headers } from "next/headers";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useMemo, useState } from "react";
 
 type Props = {
-    customerId: string
-    planExists: string
-}
+  customerId: string
+  planExists: string
+};
 
-const SubscriptionFormWrapper = (props: Props) => {
-    const {data, setClose} = useModal()
-    const router = useRouter()
-    const [selectedPriceId, setSelectedPriceId] = useState<Plan | ''>(
-        data?.plans?.defaultPriceId || ''
-    )
-    const [subscription, setSubscription] = useState<{
-        subscriptionId: string
-        clientSecret: string
-      }>({ subscriptionId: '', clientSecret: '' })
+const SubscriptionFormWrapper = ({customerId, planExists}: Props) => {
+  const { data, setClose } = useModal();
+  const router = useRouter();
+  const [selectedPriceId, setSelectedPriceId] = useState<Plan | "">(
+    data?.plans?.defaultPriceId || ""
+  );
+  const [subscription, setSubscription] = useState<{
+    subscriptionId: string;
+    clientSecret: string;
+  }>({ subscriptionId: "", clientSecret: "" });
 
-    
-      const options: StripeElementsOptions = useMemo(
-        () => ({
-            clientSecret: subscription?.clientSecret,
-            appearance: {
-              theme: 'flat',
-            },
-          }),
-        [subscription]
-      )
-  return (
-    <div>SubscriptionFormWrapper</div>
-  )
-}
+  const options: StripeElementsOptions = useMemo(
+    () => ({
+      clientSecret: subscription?.clientSecret,
+      appearance: {
+        theme: "flat",
+      },
+    }),
+    [subscription]
+  );
 
-export default SubscriptionFormWrapper
+  useEffect(()=>{
+    if(!selectedPriceId) return
+    const createSecret = async () => {
+        const subscriptionResponse = await fetch(
+            '/api/stripe/create-subscription',
+            {
+                method: 'POST',
+                headers: {
+                    "Content-type":"application/json"
+                },
+                body: JSON.stringify({
+                    customerId,
+                    priceId: selectedPriceId
+                })
+            }
+        )
+        const subscriptionResponseData = await subscriptionResponse.json()
+        setSubscription({
+            clientSecret: subscriptionResponseData.clientSecret,
+            subscriptionId: subscriptionResponseData.subscriptionId
+        })
+        if(planExists){
+            toast({
+                title: "Plan already exists.",
+                description: "Your Plan Has been successfully upgraded!"
+            })
+            setClose()
+            router.refresh()
+        }
+    }
+    createSecret()
+  },[data, selectedPriceId, customerId])
+
+  return <div>SubscriptionFormWrapper</div>;
+};
+
+export default SubscriptionFormWrapper;
