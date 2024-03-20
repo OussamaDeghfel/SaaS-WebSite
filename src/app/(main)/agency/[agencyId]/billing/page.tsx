1135eeefdc540d9d20742b40a7208d60fac11fb9
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-20 13:35:11 +0100

    find agency subscribe

--- src/app/(main)/agency/[agencyId]/billing/page.tsx
+++ src/app/(main)/agency/[agencyId]/billing/page.tsx
@@ -1,4 +1,5 @@
 import { addOnProducts } from '@/lib/constants'
+import { db } from '@/lib/db'
 import { stripe } from '@/lib/stripe'
 import React from 'react'
 
@@ -11,6 +12,18 @@ const Billing = async ({params}: Props) => {
     ids: addOnProducts.map((product) => product.id),
     expand: ['data.default_price']
   })
+
+  const agencySubscription = await db.agency.findUnique({
+    where: {
+      id: params.agencyId,
+    },
+    select: {
+      customerId: true,
+      Subscription: true
+    }
+  })
+
+  
 
   return (
     <div>Billing</div>
