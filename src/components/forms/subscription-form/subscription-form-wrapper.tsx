e1dd8d583946980389105fde6260eb2ce3874322
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-20 16:00:48 +0100

    add types and variable also useMemo function

--- src/components/forms/subscription-form/subscription-form-wrapper.tsx
+++ src/components/forms/subscription-form/subscription-form-wrapper.tsx
@@ -1,9 +1,36 @@
 "use client"
-import React from 'react'
+import { useModal } from '@/providers/modal-provider'
+import { Plan } from '@prisma/client'
+import { StripeElementsOptions } from '@stripe/stripe-js'
+import { useRouter } from 'next/navigation'
+import React, { useMemo, useState } from 'react'
 
-type Props = {}
+type Props = {
+    customerId: string
+    planExists: string
+}
 
 const SubscriptionFormWrapper = (props: Props) => {
+    const {data, setClose} = useModal()
+    const router = useRouter()
+    const [selectedPriceId, setSelectedPriceId] = useState<Plan | ''>(
+        data?.plans?.defaultPriceId || ''
+    )
+    const [subscription, setSubscription] = useState<{
+        subscriptionId: string
+        clientSecret: string
+      }>({ subscriptionId: '', clientSecret: '' })
+
+    
+      const options: StripeElementsOptions = useMemo(
+        () => ({
+            clientSecret: subscription?.clientSecret,
+            appearance: {
+              theme: 'flat',
+            },
+          }),
+        [subscription]
+      )
   return (
     <div>SubscriptionFormWrapper</div>
   )
