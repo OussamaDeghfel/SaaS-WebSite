df52126e0c6545b11a0797ae2d2364931130ba62
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-23 11:38:47 +0100

    Stripe start button edit functionality

--- src/app/(main)/agency/[agencyId]/launchpad/page.tsx
+++ src/app/(main)/agency/[agencyId]/launchpad/page.tsx
@@ -1,28 +1,34 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { db } from '@/lib/db'
-import { stripe } from '@/lib/stripe'
-import { getStripeOAuthLink } from '@/lib/utils'
-import { CheckCircle } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { db } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
+import { getStripeOAuthLink } from "@/lib/utils";
+import { CheckCircle } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 type Props = {
   params: {
     agencyId: string;
-  }
+  };
   searchParams: {
-    code : string
-  }
-}
+    code: string;
+  };
+};
 
-const LaunchPadPage = async ({params, searchParams}: Props) => {
+const LaunchPadPage = async ({ params, searchParams }: Props) => {
   const agencyDetails = await db.agency.findUnique({
-    where: {id: params.agencyId},
-  })
+    where: { id: params.agencyId },
+  });
 
-  if(!agencyDetails) return 
+  if (!agencyDetails) return;
 
   const allDetailsExist =
     agencyDetails.address &&
@@ -34,92 +40,112 @@ const LaunchPadPage = async ({params, searchParams}: Props) => {
     agencyDetails.country &&
     agencyDetails.name &&
     agencyDetails.state &&
-    agencyDetails.zipCode
-
+    agencyDetails.zipCode;
 
   const stripeOAuthLink = getStripeOAuthLink(
     "agency",
     `lunchpad___${agencyDetails.id}`
-  )
+  );
 
-  let ConnectedStripeAccount = false 
+  let ConnectedStripeAccount = false;
 
-  if(searchParams.code){
-    if(!agencyDetails.connectAccountId){
+  if (searchParams.code) {
+    if (!agencyDetails.connectAccountId) {
       try {
         const response = await stripe.oauth.token({
-          grant_type: 'authorization_code',
+          grant_type: "authorization_code",
           code: searchParams.code,
-        })
+        });
         await db.agency.update({
           where: { id: params.agencyId },
           data: { connectAccountId: response.stripe_user_id },
-        })
-        ConnectedStripeAccount = true
+        });
+        ConnectedStripeAccount = true;
       } catch (error) {
-        console.log("🔴Error connecting Stripe account")
+        console.log("Error connecting Stripe account");
       }
     }
   }
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <div className='w-full h-full max-w-[800px]'>
-        <Card className='border-none'>
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-full h-full max-w-[800px]">
+        <Card className="border-none">
           <CardHeader>
             <CardTitle>Lets Get Started</CardTitle>
-            <CardDescription>Follow the steps bellow to get your account setup</CardDescription>
+            <CardDescription>
+              Follow the steps bellow to get your account setup
+            </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-col gap-4">
-            <div className='flex justify-between items-center w-full border p-4 rounded-lg gap-2'>
-              <div className='flex gap-4 flex-col md:items-center md:!flex-row'>
-                <Image 
-                  src='/appstore.png'
-                  alt='app logo'
+            <div className="flex justify-between items-center w-full border p-4 rounded-lg gap-2">
+              <div className="flex gap-4 flex-col md:items-center md:!flex-row">
+                <Image
+                  src="/appstore.png"
+                  alt="app logo"
                   height={80}
                   width={80}
-                  className='rounded-md object-contain'
+                  className="rounded-md object-contain"
                 />
                 <p>save the website as a shortcut on your mobile device</p>
               </div>
               <Button>Start</Button>
             </div>
-            <div className='flex justify-between items-center w-full border p-4 rounded-lg gap-2'>
-              <div className='flex gap-4 flex-col md:items-center md:!flex-row'>
-                <Image 
-                  src='/stripelogo.png'
-                  alt='app logo'
+            <div className="flex justify-between items-center w-full border p-4 rounded-lg gap-2">
+              <div className="flex gap-4 flex-col md:items-center md:!flex-row">
+                <Image
+                  src="/stripelogo.png"
+                  alt="app logo"
                   height={80}
                   width={80}
-                  className='rounded-md object-contain'
+                  className="rounded-md object-contain"
                 />
-                <p>Connect your stripe account to accept payments and see your
-                  dashboard.</p>
+                <p>
+                  Connect your stripe account to accept payments and see your
+                  dashboard.
+                </p>
               </div>
-              <Button>Start</Button>
+              {agencyDetails.connectAccountId || ConnectedStripeAccount ? (
+                <CheckCircle size={50} className="text-primary flex-shrink-0" />
+              ) : (
+                <Link
+                  href={stripeOAuthLink}
+                  className="bg-primary py-2 px-4 rounded-md text-white"
+                >
+                  Start
+                </Link>
+              )}
             </div>
-            <div className='flex justify-between items-center w-full border p-4 rounded-lg gap-2'>
-              <div className='flex gap-4 flex-col md:items-center md:!flex-row'>
-                <Image 
+            <div className="flex justify-between items-center w-full border p-4 rounded-lg gap-2">
+              <div className="flex gap-4 flex-col md:items-center md:!flex-row">
+                <Image
                   src={agencyDetails.agencyLogo}
-                  alt='app logo'
+                  alt="app logo"
                   height={80}
                   width={80}
-                  className='rounded-md object-contain'
+                  className="rounded-md object-contain"
                 />
                 <p>Fill in all your bussiness details</p>
               </div>
-              {allDetailsExist 
-              ? <CheckCircle size={50} className='text-primary p-2 flex-shrink-0' /> 
-              : <Link
-                 className='bg-primary py-2 px-4 rounded-md text-white'
-                 href={`/agency/${params.agencyId}/settings`}>Start</Link>}
+              {allDetailsExist ? (
+                <CheckCircle
+                  size={50}
+                  className="text-primary p-2 flex-shrink-0"
+                />
+              ) : (
+                <Link
+                  className="bg-primary py-2 px-4 rounded-md text-white"
+                  href={`/agency/${params.agencyId}/settings`}
+                >
+                  Start
+                </Link>
+              )}
             </div>
           </CardContent>
         </Card>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LaunchPadPage
+export default LaunchPadPage;
