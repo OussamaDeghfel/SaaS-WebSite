229f0b09e7b62bc5b02a87daa144445c36510748
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-23 13:17:40 +0100

    simple edit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,5 +10,4 @@ export function getStripeOAuthLink(
   state: string
 ){
   return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&scope=read_write&redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&state=${state}`
-
 }
