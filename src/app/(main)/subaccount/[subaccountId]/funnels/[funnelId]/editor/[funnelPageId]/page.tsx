675b926149fe551ace9ac0d49b83d0380944a00d
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-25 12:36:45 +0100

    get funnel details and return to funnel page if there's nothing

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -1,8 +1,22 @@
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    params: {
+        subaccountId:string
+        funnelId:string
+        funnelPageId:string
+    }
+}
 
-const page = (props: Props) => {
+const page = async ({params}: Props) => {
+    const funnelPageDetails = await db.funnelPage.findFirst({
+        where: { id : params.funnelPageId }
+    })
+    if(!funnelPageDetails){
+        return redirect(`/subaccount/${params.subaccountId}/funnel/${params.funnelPageId}`)
+    } 
   return (
     <div>page</div>
   )
