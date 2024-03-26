d2663ae22aaec35be67e6d15eed81000f3dccf5d
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-26 17:47:17 +0100

    define the intial state for editor

--- src/providers/editor/editor-provider.tsx
+++ src/providers/editor/editor-provider.tsx
@@ -1,30 +1,53 @@
-import { EditorBtns } from "@/lib/constants"
+import { EditorBtns } from "@/lib/constants";
 
-export type DeviceTypes = "Device" | "Mobile" | "tablet"
+export type DeviceTypes = "Desktop" | "Mobile" | "tablet";
 
 export type EditorElement = {
-    id: string 
-    styles: React.CSSProperties
-    name: string 
-    type: EditorBtns
-    content: EditorElement | {}
-}
+  id: string;
+  styles: React.CSSProperties;
+  name: string;
+  type: EditorBtns;
+  content: EditorElement | {};
+};
 
-export type Editor = { 
-    liveMode: boolean
-    elements: EditorElement[]
-    selectedElement: EditorElement
-    device: DeviceTypes
-    previewMode: boolean
-    funnelPageId: string
-}
+export type Editor = {
+  liveMode: boolean;
+  elements: EditorElement[];
+  selectedElement: EditorElement;
+  device: DeviceTypes;
+  previewMode: boolean;
+  funnelPageId: string;
+};
 
 export type HistoryState = {
-    history: Editor[]
-    currentIndex: string
-}
+  history: Editor[];
+  currentIndex: string;
+};
 
 export type EditorState = {
-    editor: Editor
-    history: HistoryState
-}
+  editor: Editor;
+  history: HistoryState;
+};
+
+const InitialEditorState: EditorState["editor"] = {
+  elements: [
+    {
+      content: [],
+      id: "__body",
+      name: "Body",
+      styles: {},
+      type: "__body",
+    },
+  ],
+  selectedElement: {
+    content: [],
+    id: "",
+    name: "",
+    styles: {},
+    type: null,
+  },
+  device: 'Desktop',
+  previewMode: false,
+  liveMode: false,
+  funnelPageId: ""
+};
