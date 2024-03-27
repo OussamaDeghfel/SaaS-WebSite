477489a38f20f57d6584fa05e2853f26bb091e36
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-27 18:02:55 +0100

    functoin for save onblur Change title

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-navigation.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-navigation.tsx
@@ -1,12 +1,15 @@
 "use client";
+import { Input } from "@/components/ui/input";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { upsertFunnelPage } from "@/lib/queries";
 import { useEditor } from "@/providers/editor/editor-provider";
 import { FunnelPage } from "@prisma/client";
 import clsx from "clsx";
 import { ArrowLeftCircle } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { FocusEventHandler, useEffect } from "react";
+import { toast } from "sonner";
 
 type Props = {
   funnelId: string;
@@ -29,6 +32,31 @@ const FunnelEditorNavigation = ({
         })
     },[funnelPageDetails])
 
+    const handleOnBlurTitleChange:FocusEventHandler<HTMLInputElement> = async(event) => {
+        if(event.target.value === funnelPageDetails.name) return 
+        if(event.target.value){
+            await upsertFunnelPage(
+                subaccountId,
+                {
+                    id: funnelPageDetails.id,
+                    name: event.target.value,
+                    order: funnelPageDetails.order
+                },
+                funnelId
+            )
+            toast('Success', {
+                description: 'Saved Funnel Page title',
+              })
+            router.refresh()
+        } else {
+            toast('Oppse!', {
+                description: 'You need to have a title!',
+            })
+
+            event.target.value === funnelPageDetails.name
+        }
+    }
+
   return (
     <TooltipProvider>
         <nav
@@ -41,6 +69,16 @@ const FunnelEditorNavigation = ({
                 <Link href={`/subaccount/${subaccountId}/funnels/${funnelId}`}>
                 <ArrowLeftCircle />
                 </Link>
+                <div className="flex flex-col w-full">
+                    <Input 
+                        defaultValue={funnelPageDetails.name}
+                        className="border-none h-5 m-0 p-0 text-lg"
+                        onBlur={handleOnBlurTitleChange}
+                    />
+                    <span className="text-sm text-muted-foreground">
+                        Path: /{funnelPageDetails.pathName}
+                    </span>
+                </div>
             </aside>
         </nav>
     </TooltipProvider>
