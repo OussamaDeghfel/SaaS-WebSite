1c790ce2e1c5f196482b7bdbe1568e8129fd48f4
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-27 15:56:06 +0100

    add arrowleft to go back to funnleId inside nav inside toottipProvider component

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-navigation.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-navigation.tsx
@@ -1,8 +1,12 @@
 "use client";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { useEditor } from "@/providers/editor/editor-provider";
 import { FunnelPage } from "@prisma/client";
+import clsx from "clsx";
+import { ArrowLeftCircle } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 
 type Props = {
   funnelId: string;
@@ -17,7 +21,28 @@ const FunnelEditorNavigation = ({
 }: Props) => {
     const router = useRouter()
     const {state, dispatch} = useEditor()
-  return <div>FunnelEditorNavigation</div>;
+
+    useEffect(()=>{
+        dispatch({
+            type: 'SET_FUNNELPAGE_ID',
+            payload: { funnelPageId: funnelPageDetails.id }
+        })
+    },[funnelPageDetails])
+
+  return (
+    <TooltipProvider>
+        <nav className={clsx(
+            'border-b-[1px] flex items-center justify-between p-6 gap-2 transition-all',
+            {'!h-0 !p-0 !overflow-hidden': state.editor.previewMode}
+        )}>
+            <aside className="flex items-center gap-4 max-w-[260px] w-[300px]">
+                <Link href={`/subaccount/${subaccountId}/funnels/${funnelId}`}>
+                <ArrowLeftCircle />
+                </Link>
+            </aside>
+        </nav>
+    </TooltipProvider>
+  );
 };
 
 export default FunnelEditorNavigation;
