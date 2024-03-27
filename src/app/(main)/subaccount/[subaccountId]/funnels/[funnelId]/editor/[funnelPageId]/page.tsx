6d738c1343930da7e2e52efc0cfc9910c58d350b
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-27 15:35:55 +0100

    correct the redirection link

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -17,10 +17,10 @@ const page = async ({params}: Props) => {
         where: { id : params.funnelPageId }
     })
     if(!funnelPageDetails){
-        return redirect(`/subaccount/${params.subaccountId}/funnel/${params.funnelPageId}`)
+        return redirect(`/subaccount/${params.subaccountId}/funnels/${params.funnelId}`)
     } 
   return (
-    <div className='fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden'>
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden">
         <EditorProvider 
             subaccountId={params.subaccountId}
             funnelId={params.funnelId}
