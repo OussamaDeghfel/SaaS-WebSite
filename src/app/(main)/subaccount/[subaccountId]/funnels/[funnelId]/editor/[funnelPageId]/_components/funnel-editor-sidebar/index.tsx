374eea924b8d4214f8ced9e28b54750552eb07ed
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-28 15:52:10 +0100

    import tab list and add anothe sheetcontent to show it inside the first sheetcontent

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/index.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/index.tsx
@@ -2,7 +2,9 @@
 import { Sheet, SheetContent } from '@/components/ui/sheet'
 import { Tabs } from '@/components/ui/tabs'
 import { useEditor } from '@/providers/editor/editor-provider'
+import clsx from 'clsx'
 import React from 'react'
+import TabList from './tabs'
 
 type Props = {
     subaccountId: string
@@ -17,9 +19,24 @@ const FunnelEditorSideBar = ({subaccountId}: Props) => {
     >
         <Tabs className='w-full' defaultValue='Settings'>
             <SheetContent
-                
+                showX={false}
+                side='right'
+                 className={clsx(
+                     'mt-[97px] w-16 z-[80] shadow-none p-0 focus:border-none transition-all overflow-hidden',
+                     {hidden: state.editor.previewMode}
+                 )}
                 >
-
+                    <TabList />
+            </SheetContent>
+            <SheetContent
+                showX={false}
+                side='right'
+                 className={clsx(
+                     'mt-[97px] w-80 z-[40] shadow-none p-0 mr-16 bg-background h-full transition-all overflow-hidden',
+                     {hidden: state.editor.previewMode}
+                 )}
+                >
+                    
             </SheetContent>
         </Tabs>
     </Sheet>
