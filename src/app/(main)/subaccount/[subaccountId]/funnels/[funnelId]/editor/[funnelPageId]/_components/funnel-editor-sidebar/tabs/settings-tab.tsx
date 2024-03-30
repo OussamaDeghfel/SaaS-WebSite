c32fc00225b6e72cfab5881eb3837bc5e33d139a
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-03-30 13:49:29 +0100

    add opacity label

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/settings-tab.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/settings-tab.tsx
@@ -1,58 +1,63 @@
 "use client";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 import { EditorElement, useEditor } from "@/providers/editor/editor-provider";
 import React from "react";
 
 type Props = {};
 
-
 const SettingsTab = (props: Props) => {
-    const {state, dispatch} = useEditor()
+  const { state, dispatch } = useEditor();
 
-    const handleChangeCustomValues = (e:any) => {
-        const settingProperty = e.target.id
-        let value = e.target.value
-        const styleObject = {
-            [settingProperty]: value
-        }
+  const handleChangeCustomValues = (e: any) => {
+    const settingProperty = e.target.id;
+    let value = e.target.value;
+    const styleObject = {
+      [settingProperty]: value,
+    };
 
-        dispatch({
-            type:"UPDATE_ELEMENT",
-            payload: {
-                elementDetails: {
-                    ...state.editor.selectedElement,
-                    content: {
-                        ...state.editor.selectedElement.content,
-                        ...styleObject
-                    }
-                }
-            }
-        })
-    }
+    dispatch({
+      type: "UPDATE_ELEMENT",
+      payload: {
+        elementDetails: {
+          ...state.editor.selectedElement,
+          content: {
+            ...state.editor.selectedElement.content,
+            ...styleObject,
+          },
+        },
+      },
+    });
+  };
 
-    const handleOnChange = (e:any) => {
-        const settingProperty = e.target.id
-        let value = e.target.value
+  const handleOnChange = (e: any) => {
+    const settingProperty = e.target.id;
+    let value = e.target.value;
 
-        const styleObject = {
-            [settingProperty] : value
-        }
+    const styleObject = {
+      [settingProperty]: value,
+    };
 
-        dispatch({
-            type: 'UPDATE_ELEMENT',
-            payload: {
-                elementDetails: {
-                    ...state.editor.selectedElement,
-                    styles: {
-                        ...state.editor.selectedElement.styles,
-                        ...styleObject
-                    }
-                }
-            }
-        })
-    }
+    dispatch({
+      type: "UPDATE_ELEMENT",
+      payload: {
+        elementDetails: {
+          ...state.editor.selectedElement,
+          styles: {
+            ...state.editor.selectedElement.styles,
+            ...styleObject,
+          },
+        },
+      },
+    });
+  };
 
   return (
     <Accordion
@@ -60,43 +65,46 @@ const SettingsTab = (props: Props) => {
       className="w-full"
       defaultValue={["typography", "Dimensions", "Decoration", "FlexBox"]}
     >
-      <AccordionItem 
-        value="custom" 
-        className="px-6 py-0"
-        >
-            <AccordionTrigger className="!no-underline">Custom</AccordionTrigger>
-            <AccordionContent>
-                {state.editor.selectedElement.type === "link" && 
-                    !Array.isArray(state.editor.selectedElement.content) && (
-                        <div className="flex flex-col gap-2">
-                            <p className="text-muted-foreground">Link Path</p>
-                            <Input 
-                                id='href'
-                                placeholder="https:domain.example.com/pathname"
-                                onChange={handleChangeCustomValues}
-                                value={state.editor.selectedElement.content.href}
-                                />
-                        </div>
-                    )
-                }
-            </AccordionContent>
-        </AccordionItem>
-        <AccordionItem
-            value="Typography"
-            className="px-6 py-0 border-y-[1px]"
-            >
-                <AccordionTrigger className="!no-underline">Typography</AccordionTrigger>
-                <AccordionContent>
-                    <div className="flex flex-col gap-2">
-                        <p className="text-muted-foreground">Color</p>
-                        <Input 
-                            id="color"
-                            onChange={handleOnChange}
-                            value={state.editor.selectedElement.styles.color}
-                        />
-                    </div>
-                </AccordionContent>
-        </AccordionItem>
+      <AccordionItem value="custom" className="px-6 py-0">
+        <AccordionTrigger className="!no-underline">Custom</AccordionTrigger>
+        <AccordionContent>
+          {state.editor.selectedElement.type === "link" &&
+            !Array.isArray(state.editor.selectedElement.content) && (
+              <div className="flex flex-col gap-2">
+                <p className="text-muted-foreground">Link Path</p>
+                <Input
+                  id="href"
+                  placeholder="https:domain.example.com/pathname"
+                  onChange={handleChangeCustomValues}
+                  value={state.editor.selectedElement.content.href}
+                />
+              </div>
+            )}
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="Typography" className="px-6 py-0 border-y-[1px]">
+        <AccordionTrigger className="!no-underline">
+          Typography
+        </AccordionTrigger>
+        <AccordionContent>
+          <div className="flex flex-col gap-2">
+            <p className="text-muted-foreground">Color</p>
+            <Input
+              id="color"
+              onChange={handleOnChange}
+              value={state.editor.selectedElement.styles.color}
+            />
+          </div>
+          <div>
+            <Label className="text-muted-foreground">Opacity</Label>
+            <div className="flex items-center justify-end">
+                <small className="p-2">
+                    
+                </small>
+            </div>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
     </Accordion>
   );
 };
