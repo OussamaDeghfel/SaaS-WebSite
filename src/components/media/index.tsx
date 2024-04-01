9550fa9c05c80105492b4c0aa266fb9e7db90bc2
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-01 15:50:57 +0100

    style text edit

--- src/components/media/index.tsx
+++ src/components/media/index.tsx
@@ -14,7 +14,7 @@ const MediaComponent = ({data, subaccountId}: Props) => {
   return (
     <div className='h-full w-full flex gap-4 flex-col'>
         <div className='flex justify-between items-center'>
-            <h1 className='text-4xl'>Media Bucket</h1>
+            <h1 className=' flex text-xl'>Media Bucket</h1>
             <MediaUploadbutton subaccountId={subaccountId} />
         </div>
         <Command className='bg-transparent'>
