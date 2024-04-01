9d745311dacb858641a37d2df23a4fd1f9a2868d
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-01 12:26:05 +0100

    add Eyeoff for close the preview mode with function just send a dispatch

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/index.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/index.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { Button } from '@/components/ui/button';
 import { getFunnelPageDetails } from '@/lib/queries';
 import { useEditor } from '@/providers/editor/editor-provider'
 import clsx from 'clsx';
+import { EyeOff } from 'lucide-react';
 import React, { useEffect } from 'react'
 
 type Props = {
@@ -39,6 +41,18 @@ const FunnelEditor = ({funnelPageId, liveMode}: Props) => {
         fetchData()
     },[funnelPageId])
 
+    const handleClick = () => {
+        dispatch({
+            type: 'CHANGE_CLICKED_ELEMENT',
+            payload: {}
+        })
+    }
+
+    const handleUnPreview = () => {
+        dispatch({type: 'TOGGLE_LIVE_MODE'})
+        dispatch({type: 'TOGGLE_PREVIEW_MODE'})
+    }
+
   return (
     <div
         className={clsx(
@@ -50,7 +64,19 @@ const FunnelEditor = ({funnelPageId, liveMode}: Props) => {
                 'w-full': state.editor.device === 'Desktop',
             }
         )}
-    ></div>
+        onClick={handleClick}
+    >
+        {state.editor.previewMode && state.editor.liveMode && 
+            <Button 
+                variant={'ghost'}
+                size={'icon'}
+                className='w-6 h-6 bg-slate-600 p-[2px] fixed top-0 left-0 z-[100]'
+                onClick={handleUnPreview}
+                >
+                <EyeOff />
+            </Button>
+        }
+    </div>
   )
 }
 
