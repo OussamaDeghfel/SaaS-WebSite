32329c6dae706371e2d20a0d04235e743ab2d7d1
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-03 15:26:07 +0100

    edit style of clsx

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
@@ -88,22 +88,23 @@ const Container = ({ element }: Props) => {
   return (
     <div
       style={styles}
-      className={clsx("relative p-4 transition-all group", {
-        "max-w-full w-full": type === "container" || type === "2Col",
-        "h-fit": type === "container",
-        "h-full": type === "2Col",
-        "flex flex-col !md:flex-row": type === "2Col",
-        "!border-blue-500":
+      className={clsx('relative p-4 transition-all group', {
+        'max-w-full w-full': type === 'container' || type === '2Col',
+        'h-fit': type === 'container',
+        'h-full': type === '__body',
+        'overflow-scroll ': type === '__body',
+        'flex flex-col md:!flex-row': type === '2Col',
+        '!border-blue-500':
           state.editor.selectedElement.id === id &&
           !state.editor.liveMode &&
-          state.editor.selectedElement.type !== "__body",
-        "!border-yellow-400 !border-4":
+          state.editor.selectedElement.type !== '__body',
+        '!border-yellow-400 !border-4':
           state.editor.selectedElement.id === id &&
           !state.editor.liveMode &&
-          state.editor.selectedElement.type === "__body",
-        "!border-solid":
+          state.editor.selectedElement.type === '__body',
+        '!border-solid':
           state.editor.selectedElement.id === id && !state.editor.liveMode,
-        "border-dashed border-[1px] border-slate-300": !state.editor.liveMode,
+        'border-dashed border-[1px] border-slate-300': !state.editor.liveMode,
       })}
       onDrop={(e) => handleOnDrop(e, id)}
       onDragOver={handleDragOver}
