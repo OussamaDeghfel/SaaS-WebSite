0f28ae9d85fcee53a4458274c686d8522195bb4c
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-03 13:38:39 +0100

    define styles and add different class style for each type also define the function of Drop content

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { EditorElement, useEditor } from '@/providers/editor/editor-provider'
+import clsx from 'clsx'
 import React from 'react'
 
 type Props = {
@@ -8,8 +9,32 @@ type Props = {
 
 const Container = ({element:{id,name,styles,content,type}}: Props) => {
     const {state, dispatch} = useEditor()
+
+    const handleOnDrop = () => {}
   return (
-    <div>Container</div>
+    <div 
+      style={styles}
+      className={clsx(
+        'relative p-4 transition-all group', {
+          'max-w-full w-full': type === 'container' || type === '2Col',
+          'h-fit': type === 'container',
+          'h-full': type === '2Col',
+          'flex flex-col !md:flex-row': type === '2Col',
+          '!border-blue-500': 
+            state.editor.selectedElement.id === id && 
+            !state.editor.liveMode && 
+            state.editor.selectedElement.type !== '__body',
+          '!border-yellow-400 !border-4': 
+            state.editor.selectedElement.id === id && 
+            !state.editor.liveMode && 
+            state.editor.selectedElement.type === '__body',
+          '!border-solid': 
+            state.editor.selectedElement.id === id && !state.editor.liveMode,
+          'border-dashed border-[1px] border-slate-300': !state.editor.liveMode
+         }
+      )}
+      onDrop={(e) => handleOnDrop(e, id)}
+      >Container</div>
   )
 }
 
