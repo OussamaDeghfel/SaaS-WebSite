6bdf6be5121ad6a11dd2167502d1d2a2c0c688fc
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-03 13:48:13 +0100

    descript the dispatch of add element when we drop

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
@@ -1,7 +1,9 @@
 'use client'
+import { EditorBtns, defaultStyles } from '@/lib/constants'
 import { EditorElement, useEditor } from '@/providers/editor/editor-provider'
 import clsx from 'clsx'
 import React from 'react'
+import { v4 } from 'uuid'
 
 type Props = {
     element: EditorElement
@@ -10,7 +12,30 @@ type Props = {
 const Container = ({element:{id,name,styles,content,type}}: Props) => {
     const {state, dispatch} = useEditor()
 
-    const handleOnDrop = () => {}
+    const handleOnDrop = (e: React.DragEvent, type: string) => {
+      const componentType = e.dataTransfer.getData('componentType') as EditorBtns
+      switch (componentType) {
+        case 'text':
+            dispatch({
+              type: 'ADD_ELEMENT',
+              payload: ({
+                containerId: id,
+                elementDetails: {
+                  content : {innerText: 'Text Element'},
+                  id: v4(),
+                  name: 'Text',
+                  styles: {
+                    color: 'black',
+                    ...defaultStyles
+                  },
+                  type: 'text'
+                }
+              })
+            })
+        default:
+          break;
+      }
+    }
   return (
     <div 
       style={styles}
