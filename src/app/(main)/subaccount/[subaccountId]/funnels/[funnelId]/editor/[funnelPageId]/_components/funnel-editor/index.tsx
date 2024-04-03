548cebe68fcdfec920d316d215e16a79362f473d
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-03 15:26:44 +0100

    add the Recursive component to map through element

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/index.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/index.tsx
@@ -1,83 +1,90 @@
-'use client'
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { getFunnelPageDetails } from '@/lib/queries';
-import { useEditor } from '@/providers/editor/editor-provider'
-import clsx from 'clsx';
-import { EyeOff } from 'lucide-react';
-import React, { useEffect } from 'react'
+import { Button } from "@/components/ui/button";
+import { getFunnelPageDetails } from "@/lib/queries";
+import { useEditor } from "@/providers/editor/editor-provider";
+import clsx from "clsx";
+import { EyeOff } from "lucide-react";
+import React, { useEffect } from "react";
+import Recursive from "./funnel-editor-components/recursive";
 
 type Props = {
-    funnelPageId: string; liveMode?: boolean
-}
+  funnelPageId: string;
+  liveMode?: boolean;
+};
 
-const FunnelEditor = ({funnelPageId, liveMode}: Props) => {
-    const {state, dispatch} = useEditor()
+const FunnelEditor = ({ funnelPageId, liveMode }: Props) => {
+  const { state, dispatch } = useEditor();
 
-    useEffect(()=>{
-        if(liveMode){
-            dispatch({
-                type: 'TOGGLE_LIVE_MODE',
-                payload: {value: true}
-            })
-        }
-    },[liveMode])
-
-    useEffect(()=> {
-        const fetchData = async()=>{
-            const response = await getFunnelPageDetails(funnelPageId)
-            if(!response) return 
-
-            console.log(response)
-
-            dispatch({
-                type: 'LOAD_DATA',
-                payload: {
-                    elements: response.content ? JSON.parse(response?.content) : '',
-                    withLive: !!liveMode
-                }
-            })
-        }
-        fetchData()
-    },[funnelPageId])
-
-    const handleClick = () => {
-        dispatch({
-            type: 'CHANGE_CLICKED_ELEMENT',
-            payload: {}
-        })
+  useEffect(() => {
+    if (liveMode) {
+      dispatch({
+        type: "TOGGLE_LIVE_MODE",
+        payload: { value: true },
+      });
     }
+  }, [liveMode]);
 
-    const handleUnPreview = () => {
-        dispatch({type: 'TOGGLE_LIVE_MODE'})
-        dispatch({type: 'TOGGLE_PREVIEW_MODE'})
-    }
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getFunnelPageDetails(funnelPageId);
+      if (!response) return;
+
+      console.log(response);
+
+      dispatch({
+        type: "LOAD_DATA",
+        payload: {
+          elements: response.content ? JSON.parse(response?.content) : "",
+          withLive: !!liveMode,
+        },
+      });
+    };
+    fetchData();
+  }, [funnelPageId]);
+
+  const handleClick = () => {
+    dispatch({
+      type: "CHANGE_CLICKED_ELEMENT",
+      payload: {},
+    });
+  };
+
+  const handleUnPreview = () => {
+    dispatch({ type: "TOGGLE_LIVE_MODE" });
+    dispatch({ type: "TOGGLE_PREVIEW_MODE" });
+  };
 
   return (
     <div
-        className={clsx(
-            'use-automation-zoom-in h-full overflow-scroll mr-[385px] bg-background transition-all rounded-md',
-            {
-                '!pr-0 !mr-0' : state.editor.previewMode === true || state.editor.liveMode === true,
-                '!w-[850px]': state.editor.device === 'Tablet',
-                '!w-[420px]': state.editor.device === 'Mobile',
-                'w-full': state.editor.device === 'Desktop',
-            }
-        )}
-        onClick={handleClick}
-    >
-        {state.editor.previewMode && state.editor.liveMode && 
-            <Button 
-                variant={'ghost'}
-                size={'icon'}
-                className='w-6 h-6 bg-slate-600 p-[2px] fixed top-0 left-0 z-[100]'
-                onClick={handleUnPreview}
-                >
-                <EyeOff />
-            </Button>
+      className={clsx(
+        "use-automation-zoom-in h-full overflow-scroll mr-[385px] bg-background transition-all rounded-md",
+        {
+          "!pr-0 !mr-0":
+            state.editor.previewMode === true || state.editor.liveMode === true,
+          "!w-[850px]": state.editor.device === "Tablet",
+          "!w-[420px]": state.editor.device === "Mobile",
+          "w-full": state.editor.device === "Desktop",
         }
+      )}
+      onClick={handleClick}
+    >
+      {state.editor.previewMode && state.editor.liveMode && (
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          className="w-6 h-6 bg-slate-600 p-[2px] fixed top-0 left-0 z-[100]"
+          onClick={handleUnPreview}
+        >
+          <EyeOff />
+        </Button>
+      )}
+      {Array.isArray(state.editor.elements) &&
+        state.editor.elements.map((childElement) => (
+          <Recursive key={childElement.id} element={childElement} />
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default FunnelEditor
+export default FunnelEditor;
