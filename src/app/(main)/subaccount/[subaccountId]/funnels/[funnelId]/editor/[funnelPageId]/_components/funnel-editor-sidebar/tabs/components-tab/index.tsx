a77e54cb051ea50e2c82b79c4d123f4ee59caaf6
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-04 13:19:50 +0100

    define element variable for each button component, add accordion for show layout

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/components-tab/index.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/components-tab/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { EditorBtns } from "@/lib/constants";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const ComponentsTab = (props: Props) => {
+  const elements: {
+    Component: React.ReactNode;
+    label: string;
+    id: EditorBtns;
+    group: "layout" | "element";
+  }[] = [];
   return (
-    <div>ComponentsTab</div>
-  )
-}
+    <Accordion
+      type="multiple"
+      className="w-full"
+      defaultValue={["Layout", "Elements"]}
+    >
+      <AccordionItem value="Layout" className="px-6 py-0 border-y-[1px]">
+        <AccordionTrigger className="!no-underline">Layout</AccordionTrigger>
+        <AccordionContent className="flex flex-wrap gap-2">
+          {elements
+            .filter((element) => element.group === "layout")
+            .map((element) => (
+              <div
+                key={element.id}
+                className="flex-col items-center justify-center flex"
+              >
+                {element.Component}
+                <span className="text-muted-foreground">{element.label}</span>
+              </div>
+            ))}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
 
-export default ComponentsTab
+export default ComponentsTab;
