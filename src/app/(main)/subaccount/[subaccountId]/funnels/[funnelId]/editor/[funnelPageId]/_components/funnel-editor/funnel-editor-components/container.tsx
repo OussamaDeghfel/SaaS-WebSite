9b32b83e57bb8712ab649c16575dde3e79de5377
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-05 14:48:50 +0100

    add link 2col cases

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
@@ -13,11 +13,11 @@ type Props = {
 };
 
 const Container = ({ element }: Props) => {
-  const { id, name, styles, content, type } = element
+  const { id, name, styles, content, type } = element;
   const { state, dispatch } = useEditor();
 
   const handleOnDrop = (e: React.DragEvent, type: string) => {
-    e.stopPropagation()
+    e.stopPropagation();
     const componentType = e.dataTransfer.getData("componentType") as EditorBtns;
 
     switch (componentType) {
@@ -58,20 +58,71 @@ const Container = ({ element }: Props) => {
         break;
       case "video":
         dispatch({
-          type: 'ADD_ELEMENT',
+          type: "ADD_ELEMENT",
           payload: {
             containerId: id,
             elementDetails: {
               content: {
-                src: 'https://www.youtube.com/embed/A3l6YYkXzzg?si=zbcCeWcpq7Cwf8W1',
+                src: "https://www.youtube.com/embed/A3l6YYkXzzg?si=zbcCeWcpq7Cwf8W1",
               },
               id: v4(),
-              name: 'Video',
+              name: "Video",
               styles: {},
-              type: 'video',
+              type: "video",
             },
           },
-        })
+        });
+        break;
+      case "link":
+        dispatch({
+          type: "ADD_ELEMENT",
+          payload: {
+            containerId: id,
+            elementDetails: {
+              content: {
+                innerText: "Link Element",
+                href: "#",
+              },
+              id: v4(),
+              name: "Link",
+              styles: {
+                color: "black",
+                ...defaultStyles,
+              },
+              type: "link",
+            },
+          },
+        });
+        break;
+      case "2Col":
+        dispatch({
+          type: "ADD_ELEMENT",
+          payload: {
+            containerId: id,
+            elementDetails: {
+              content: [
+                {
+                  content: [],
+                  id: v4(),
+                  name: "Container",
+                  styles: { ...defaultStyles, width: "100%" },
+                  type: "container",
+                },
+                {
+                  content: [],
+                  id: v4(),
+                  name: "Container",
+                  styles: { ...defaultStyles, width: "100%" },
+                  type: "container",
+                },
+              ],
+              id: v4(),
+              name: "Two Columns",
+              styles: { ...defaultStyles, display: "flex" },
+              type: "2Col",
+            },
+          },
+        });
         break;
       default:
         break;
@@ -79,61 +130,61 @@ const Container = ({ element }: Props) => {
   };
 
   const handleDragOver = (e: React.DragEvent) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
-  const handleDragStart = (e: React.DragEvent, type:string) => {
-    if(type === "__body") return 
-    e.dataTransfer.setData('componentType', type)
-  }
+  const handleDragStart = (e: React.DragEvent, type: string) => {
+    if (type === "__body") return;
+    e.dataTransfer.setData("componentType", type);
+  };
 
   const handleOnClickBody = (e: React.MouseEvent) => {
-    e.stopPropagation()
+    e.stopPropagation();
     dispatch({
-      type: 'CHANGE_CLICKED_ELEMENT',
+      type: "CHANGE_CLICKED_ELEMENT",
       payload: {
-        elementDetails: element
-      }
-    })
-  }
+        elementDetails: element,
+      },
+    });
+  };
 
   const handleDeleteElement = () => {
     dispatch({
-      type: 'DELETE_ELEMENT',
-      payload: {elementDetails: element}
-    })
-  }
+      type: "DELETE_ELEMENT",
+      payload: { elementDetails: element },
+    });
+  };
 
   return (
     <div
       style={styles}
-      className={clsx('relative p-4 transition-all group', {
-        'max-w-full w-full': type === 'container' || type === '2Col',
-        'h-fit': type === 'container',
-        'h-full': type === '__body',
-        'overflow-scroll ': type === '__body',
-        'flex flex-col md:!flex-row': type === '2Col',
-        '!border-blue-500':
+      className={clsx("relative p-4 transition-all group", {
+        "max-w-full w-full": type === "container" || type === "2Col",
+        "h-fit": type === "container",
+        "h-full": type === "__body",
+        "overflow-scroll ": type === "__body",
+        "flex flex-col md:!flex-row": type === "2Col",
+        "!border-blue-500":
           state.editor.selectedElement.id === id &&
           !state.editor.liveMode &&
-          state.editor.selectedElement.type !== '__body',
-        '!border-yellow-400 !border-4':
+          state.editor.selectedElement.type !== "__body",
+        "!border-yellow-400 !border-4":
           state.editor.selectedElement.id === id &&
           !state.editor.liveMode &&
-          state.editor.selectedElement.type === '__body',
-        '!border-solid':
+          state.editor.selectedElement.type === "__body",
+        "!border-solid":
           state.editor.selectedElement.id === id && !state.editor.liveMode,
-        'border-dashed border-[1px] border-slate-300': !state.editor.liveMode,
+        "border-dashed border-[1px] border-slate-300": !state.editor.liveMode,
       })}
       onDrop={(e) => handleOnDrop(e, id)}
       onDragOver={handleDragOver}
-      draggable={type !== '__body'}
-      onDragStart={(e) => handleDragStart(e, 'container')}
+      draggable={type !== "__body"}
+      onDragStart={(e) => handleDragStart(e, "container")}
       onClick={handleOnClickBody}
     >
       <Badge
         className={clsx(
-          'absolute -top-[23px] -left-[1px] rounded-none rounded-t-lg hidden',
+          "absolute -top-[23px] -left-[1px] rounded-none rounded-t-lg hidden",
           {
             block:
               state.editor.selectedElement.id === element.id &&
@@ -144,23 +195,18 @@ const Container = ({ element }: Props) => {
         {element.name}
       </Badge>
 
-      {Array.isArray(content) && 
+      {Array.isArray(content) &&
         content.map((childElement) => (
-          <Recursive 
-            key={childElement.id}
-            element={childElement}
-          />
-        ))
-        }
+          <Recursive key={childElement.id} element={childElement} />
+        ))}
 
-        {state.editor.selectedElement.id === id && 
-          !state.editor.liveMode && 
-          state.editor.selectedElement.type !== "__body" && (
-            <div className="absolute bg-primary px-2.5 py-1 text-xs font-bold -top-[25px] -right-[1px] rounded-none rounded-t-lg">
-              <Trash size={16} onClick={handleDeleteElement} />
-            </div>
-          )
-          }
+      {state.editor.selectedElement.id === id &&
+        !state.editor.liveMode &&
+        state.editor.selectedElement.type !== "__body" && (
+          <div className="absolute bg-primary px-2.5 py-1 text-xs font-bold -top-[25px] -right-[1px] rounded-none rounded-t-lg">
+            <Trash size={16} onClick={handleDeleteElement} />
+          </div>
+        )}
     </div>
   );
 };
