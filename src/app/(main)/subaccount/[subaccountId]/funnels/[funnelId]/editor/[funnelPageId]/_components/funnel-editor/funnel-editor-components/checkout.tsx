6e61e50db1a1c3994b385dd8b163554ee99d63a4
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-07 14:22:38 +0100

    fetch clientsecret from stripe api endpoint

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/checkout.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/checkout.tsx
@@ -1,53 +1,91 @@
-'use client'
-import SubAccountDetails from '@/components/forms/subaccount-details'
-import { getFunnel, getSubaccountDetails } from '@/lib/queries'
-import { EditorElement, useEditor } from '@/providers/editor/editor-provider'
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useMemo, useState } from 'react'
+"use client";
+import SubAccountDetails from "@/components/forms/subaccount-details";
+import { toast } from "@/components/ui/use-toast";
+import { getFunnel, getSubaccountDetails } from "@/lib/queries";
+import { EditorElement, useEditor } from "@/providers/editor/editor-provider";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useMemo, useState } from "react";
 
 type Props = {
-    element: EditorElement
-}
+  element: EditorElement;
+};
 
 const Checkout = (props: Props) => {
-    const {dispatch, state, subaccountId, funnelId, pageDetails} = useEditor() 
-    const router = useRouter()
-    const [clientSecret, setClientSecret] = useState('')
-    const [livePrices, setLivePrices] = useState([])
-    const [subaccountConnectAccId, setSubaccountConnectAccId] = useState('')
-    const options = useMemo(() => {(clientSecret)},[clientSecret])
+  const { dispatch, state, subaccountId, funnelId, pageDetails } = useEditor();
+  const router = useRouter();
+  const [clientSecret, setClientSecret] = useState("");
+  const [livePrices, setLivePrices] = useState([]);
+  const [subaccountConnectAccId, setSubaccountConnectAccId] = useState("");
+  const options = useMemo(() => {
+    clientSecret;
+  }, [clientSecret]);
 
-    useEffect(()=>{
-        if(!subaccountId) return
+  useEffect(() => {
+    if (!subaccountId) return;
 
-        const fetchData = async()=> {
-            const subaccountDetails = await getSubaccountDetails(subaccountId)
-            if(subaccountDetails){
-                if(!subaccountDetails.connectAccountId) return
-                setSubaccountConnectAccId(subaccountDetails.connectAccountId)
-            }
-        }
+    const fetchData = async () => {
+      const subaccountDetails = await getSubaccountDetails(subaccountId);
+      if (subaccountDetails) {
+        if (!subaccountDetails.connectAccountId) return;
+        setSubaccountConnectAccId(subaccountDetails.connectAccountId);
+      }
+    };
 
-        fetchData()
+    fetchData();
+  }, [subaccountId]);
 
-    },[subaccountId])
-
-    useEffect(()=>{
-        if(!funnelId){
-        const fetchData = async ()=> {
-            const funnelData = await getFunnel(funnelId)
-            setLivePrices(JSON.parse(funnelData?.liveProducts || '[]'))
-        }
-        fetchData()
-    
+  useEffect(() => {
+    if (!funnelId) {
+      const fetchData = async () => {
+        const funnelData = await getFunnel(funnelId);
+        setLivePrices(JSON.parse(funnelData?.liveProducts || "[]"));
+      };
+      fetchData();
     }
+  }, [funnelId]);
 
+  useEffect(() => {
+    if (livePrices.length && subaccountId && subaccountConnectAccId) {
+      const getClientSecret = async () => {
+        try {
+            const body = JSON.stringify({
+                subaccountConnectAccId,
+                prices: livePrices,
+                subaccountId
+            })
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_URL}api/stripe/create-checkout-session`,
+                {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body
+                }
+            )
+            const responseJson = await  response.json()
+            if(!responseJson) throw new Error('something went wrong ')
+            if(responseJson.error){
+                throw new Error(responseJson.error)
+            }
+            if(responseJson.clientSecret){
+                setClientSecret(responseJson.clientSecret)
+            }
+        } catch (error) {
+            toast({
+                open: true,
+                className: 'z-[100000]',
+                variant: "destructive",
+                title: "Oppse!",
+                //@ts-ignore
+                description: error.message,
+            })
+        }
+      };
 
-    },[funnelId])
+      getClientSecret()
+    }
+  }, [livePrices, subaccountId, subaccountConnectAccId]);
 
-  return (
-    <div>Checkout</div>
-  )
-}
+  return <div>Checkout</div>;
+};
 
-export default Checkout
+export default Checkout;
