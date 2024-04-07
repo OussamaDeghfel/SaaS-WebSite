8f16ce8365c3bfcd6f669a89a6137445ee524d32
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-07 13:07:41 +0100

    initial props and form for validation the updated changes

--- src/components/forms/contact-form.tsx
+++ src/components/forms/contact-form.tsx
@@ -1,8 +1,24 @@
+import { ContactUserFormSchema } from '@/lib/types'
+import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-type Props = {}
+type Props = {
+    title: string 
+    subTitle: string 
+    apiCall: (values: z.infer<typeof ContactUserFormSchema>) => any 
+}
 
-const ContactForm = (props: Props) => {
+const ContactForm = ({title, subTitle, apiCall}: Props) => {
+    const form = useForm<z.infer<typeof ContactUserFormSchema>>({
+        mode: 'onChange',
+        resolver: zodResolver(ContactUserFormSchema),
+        defaultValues:{
+            name:'',
+            email: ''
+        }
+    })
   return (
     <div>ContactForm</div>
   )
