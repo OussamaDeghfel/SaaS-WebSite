4a40ff9b42535a155ea0d666075c010d9bd31ada
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-08 11:28:47 +0100

    intial variable for agencyDashboard

--- src/app/(main)/agency/[agencyId]/page.tsx
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Page = ({ params }: { params: { agencyId: string } }) => {
-  return (
-    <div>{ params.agencyId }</div>
-  )
-}
+const Page = ({
+  params,
+}: {
+  params: { agencyId: string };
+  searchParams: { code: string };
+}) => {
 
-export default Page
+  let currency = 'USD'
+  let sessions
+  let totalClosedSessions
+  let totalPendingSessions
+  let net = 0
+  let potentialIncome = 0
+  let closingRate = 0 
+  const currentYear = new Date().getFullYear()
+  const startDate = new Date(`${currentYear}-01-01T00:00:00Z`).getTime() / 1000
+  const endDate = new Date(`${currentYear}-12-31T23:59:59Z`).getTime() / 1000
+
+  return <div>{params.agencyId}</div>;
+};
+
+export default Page;
