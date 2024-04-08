b36ad8c5f892adb7574ca43ac90e59fecf78d4a8
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-08 15:37:15 +0100

    change property name of value

--- src/components/global/circle-progress.tsx
+++ src/components/global/circle-progress.tsx
@@ -3,26 +3,25 @@ import { ProgressCircle } from '@tremor/react'
 import React from 'react'
 
 type Props = {
-    value: number
-    description: React.ReactNode
+  val: number
+  description: React.ReactNode
 }
 
-const CircleProgress = ({value, description}: Props) => {
-
+const CircleProgress = ({ description, val = 0 }: Props) => {
   return (
-    <div className='flex items-center gap-4'>
-        <ProgressCircle 
-            showAnimation={true}
-            value={value}
-            radius={70}
-            strokeWidth={20}
-        >
-            {value}%
-        </ProgressCircle>
-        <div>
-            <b>Closing Rate</b>
-            <p className='text-muted-foreground'>{description}</p>
-        </div>
+    <div className="flex gap-4 items-center">
+      <ProgressCircle
+        showAnimation={true}
+        value={val}
+        radius={70}
+        strokeWidth={20}
+      >
+        {val}%
+      </ProgressCircle>
+      <div>
+        <b>Closing Rate</b>
+        <p className="text-muted-foreground">{description}</p>
+      </div>
     </div>
   )
 }
