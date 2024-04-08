df312ee96e21cebc224c8063ea5309dac4e75773
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-08 15:37:30 +0100

    add circleprogress

--- src/app/(main)/agency/[agencyId]/page.tsx
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -1,3 +1,4 @@
+import CircleProgress from '@/components/global/circle-progress'
 import {
   Card,
   CardContent,
@@ -5,87 +6,94 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
-import { Separator } from "@/components/ui/separator";
-import { db } from "@/lib/db";
-import { stripe } from "@/lib/stripe";
-import { AreaChart } from "@tremor/react";
-import { ClipboardIcon, Contact2, DollarSign, Goal } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+} from '@/components/ui/card'
+import { Progress } from '@/components/ui/progress'
+import { Separator } from '@/components/ui/separator'
+import { db } from '@/lib/db'
+import { stripe } from '@/lib/stripe'
+import { AreaChart } from '@tremor/react'
+import {
+  ClipboardIcon,
+  Contact2,
+  DollarSign,
+  Goal,
+  ShoppingCart,
+} from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
 
 const Page = async ({
   params,
 }: {
-  params: { agencyId: string };
-  searchParams: { code: string };
+  params: { agencyId: string }
+  searchParams: { code: string }
 }) => {
-  let currency = "USD";
-  let sessions;
-  let totalClosedSessions;
-  let totalPendingSessions;
-  let net = 0;
-  let potentialIncome = 0;
-  let closingRate = 0;
-  const currentYear = new Date().getFullYear();
-  const startDate = new Date(`${currentYear}-01-01T00:00:00Z`).getTime() / 1000;
-  const endDate = new Date(`${currentYear}-12-31T23:59:59Z`).getTime() / 1000;
+  let currency = 'USD'
+  let sessions
+  let totalClosedSessions
+  let totalPendingSessions
+  let net = 0
+  let potentialIncome = 0
+  let closingRate = 0
+  const currentYear = new Date().getFullYear()
+  const startDate = new Date(`${currentYear}-01-01T00:00:00Z`).getTime() / 1000
+  const endDate = new Date(`${currentYear}-12-31T23:59:59Z`).getTime() / 1000
 
   const agencyDetails = await db.agency.findUnique({
-    where: { id: params.agencyId },
-  });
+    where: {
+      id: params.agencyId,
+    },
+  })
 
-  if (!agencyDetails) return;
+  if (!agencyDetails) return
 
   const subaccounts = await db.subAccount.findMany({
     where: {
       agencyId: params.agencyId,
     },
-  });
+  })
 
   if (agencyDetails.connectAccountId) {
     const response = await stripe.accounts.retrieve({
       stripeAccount: agencyDetails.connectAccountId,
-    });
+    })
 
-    currency = response.default_currency?.toUpperCase() || "USD";
+    currency = response.default_currency?.toUpperCase() || 'USD'
     const checkoutSessions = await stripe.checkout.sessions.list(
       {
         created: { gte: startDate, lte: endDate },
         limit: 100,
       },
-      {
-        stripeAccount: agencyDetails.connectAccountId,
-      }
-    );
-    sessions = checkoutSessions.data;
+      { stripeAccount: agencyDetails.connectAccountId }
+    )
+    sessions = checkoutSessions.data
     totalClosedSessions = checkoutSessions.data
-      .filter((session) => session.status === "complete")
+      .filter((session) => session.status === 'complete')
       .map((session) => ({
         ...session,
         created: new Date(session.created).toLocaleDateString(),
         amount_total: session.amount_total ? session.amount_total / 100 : 0,
-      }));
+      }))
+
     totalPendingSessions = checkoutSessions.data
-      .filter((session) => session.status === "open")
+      .filter((session) => session.status === 'open')
       .map((session) => ({
         ...session,
         created: new Date(session.created).toLocaleDateString(),
         amount_total: session.amount_total ? session.amount_total / 100 : 0,
-      }));
+      }))
     net = +totalClosedSessions
       .reduce((total, session) => total + (session.amount_total || 0), 0)
-      .toFixed(2);
+      .toFixed(2)
 
     potentialIncome = +totalPendingSessions
       .reduce((total, session) => total + (session.amount_total || 0), 0)
-      .toFixed(2);
+      .toFixed(2)
 
     closingRate = +(
       (totalClosedSessions.length / checkoutSessions.data.length) *
       100
-    ).toFixed(2);
+    ).toFixed(2)
   }
 
   return (
@@ -110,7 +118,7 @@ const Page = async ({
         </div>
       )}
       <h1 className="text-4xl">Dashboard</h1>
-      <Separator className="my-6" />
+      <Separator className=" my-6" />
       <div className="flex flex-col gap-4 pb-6">
         <div className="flex gap-4 flex-col xl:!flex-row">
           <Card className="flex-1 relative">
@@ -120,7 +128,7 @@ const Page = async ({
                 {net ? `${currency} ${net.toFixed(2)}` : `$0.00`}
               </CardTitle>
               <small className="text-xs text-muted-foreground">
-                For The Year {currentYear}
+                For the year {currentYear}
               </small>
             </CardHeader>
             <CardContent className="text-sm text-muted-foreground">
@@ -195,8 +203,8 @@ const Page = async ({
                 ...(totalPendingSessions || []),
               ]}
               index="created"
-              categories={["amount_total"]}
-              colors={["primary"]}
+              categories={['amount_total']}
+              colors={['primary']}
               yAxisWidth={30}
               showAnimation={true}
             />
@@ -206,13 +214,37 @@ const Page = async ({
               <CardTitle>Conversions</CardTitle>
             </CardHeader>
             <CardContent>
-              
+              <CircleProgress
+                value={closingRate}
+                description={
+                  <>
+                    {sessions && (
+                      <div className="flex flex-col">
+                        Abandoned
+                        <div className="flex gap-2">
+                          <ShoppingCart className="text-rose-700" />
+                          {sessions.length}
+                        </div>
+                      </div>
+                    )}
+                    {totalClosedSessions && (
+                      <div className="felx flex-col">
+                        Won Carts
+                        <div className="flex gap-2">
+                          <ShoppingCart className="text-emerald-700" />
+                          {totalClosedSessions.length}
+                        </div>
+                      </div>
+                    )}
+                  </>
+                }
+              />
             </CardContent>
           </Card>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
