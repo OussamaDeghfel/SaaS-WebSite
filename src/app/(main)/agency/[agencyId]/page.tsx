ade78dcc6264afd97c4d87777192c61cd5292071
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-08 14:47:53 +0100

    transaction history on graph using areachart react tremor library

--- src/app/(main)/agency/[agencyId]/page.tsx
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -1,8 +1,16 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
+import { AreaChart } from "@tremor/react";
 import { ClipboardIcon, Contact2, DollarSign, Goal } from "lucide-react";
 import Link from "next/link";
 import React from "react";
@@ -13,75 +21,76 @@ const Page = async ({
   params: { agencyId: string };
   searchParams: { code: string };
 }) => {
-
-  let currency = 'USD'
-  let sessions
-  let totalClosedSessions
-  let totalPendingSessions
-  let net = 0
-  let potentialIncome = 0
-  let closingRate = 0 
-  const currentYear = new Date().getFullYear()
-  const startDate = new Date(`${currentYear}-01-01T00:00:00Z`).getTime() / 1000
-  const endDate = new Date(`${currentYear}-12-31T23:59:59Z`).getTime() / 1000
+  let currency = "USD";
+  let sessions;
+  let totalClosedSessions;
+  let totalPendingSessions;
+  let net = 0;
+  let potentialIncome = 0;
+  let closingRate = 0;
+  const currentYear = new Date().getFullYear();
+  const startDate = new Date(`${currentYear}-01-01T00:00:00Z`).getTime() / 1000;
+  const endDate = new Date(`${currentYear}-12-31T23:59:59Z`).getTime() / 1000;
 
   const agencyDetails = await db.agency.findUnique({
-    where: {id: params.agencyId}
-  })
+    where: { id: params.agencyId },
+  });
 
-  if(!agencyDetails) return 
+  if (!agencyDetails) return;
 
   const subaccounts = await db.subAccount.findMany({
     where: {
-      agencyId: params.agencyId
-    }
-  })
+      agencyId: params.agencyId,
+    },
+  });
 
-  if(agencyDetails.connectAccountId){
+  if (agencyDetails.connectAccountId) {
     const response = await stripe.accounts.retrieve({
-      stripeAccount: agencyDetails.connectAccountId
-    })
+      stripeAccount: agencyDetails.connectAccountId,
+    });
 
-    currency = response.default_currency?.toUpperCase() || 'USD'
+    currency = response.default_currency?.toUpperCase() || "USD";
     const checkoutSessions = await stripe.checkout.sessions.list(
       {
-        created: {gte: startDate, lte:endDate},
-        limit: 100
+        created: { gte: startDate, lte: endDate },
+        limit: 100,
       },
       {
-        stripeAccount: agencyDetails.connectAccountId
+        stripeAccount: agencyDetails.connectAccountId,
       }
-      )
-      sessions = checkoutSessions.data
-      totalClosedSessions = checkoutSessions.data
-      .filter((session) => session.status === 'complete')
+    );
+    sessions = checkoutSessions.data;
+    totalClosedSessions = checkoutSessions.data
+      .filter((session) => session.status === "complete")
       .map((session) => ({
         ...session,
         created: new Date(session.created).toLocaleDateString(),
-        amount_total: session.amount_total ? session.amount_total / 100 : 0
-      }))
-      totalPendingSessions = checkoutSessions.data
-      .filter((session) => session.status === 'open')
+        amount_total: session.amount_total ? session.amount_total / 100 : 0,
+      }));
+    totalPendingSessions = checkoutSessions.data
+      .filter((session) => session.status === "open")
       .map((session) => ({
         ...session,
         created: new Date(session.created).toLocaleDateString(),
-        amount_total: session.amount_total ? session.amount_total / 100 : 0
-      }))
-      net = +totalClosedSessions
-        .reduce((total, session) => total + (session.amount_total || 0), 0)
-        .toFixed(2)
+        amount_total: session.amount_total ? session.amount_total / 100 : 0,
+      }));
+    net = +totalClosedSessions
+      .reduce((total, session) => total + (session.amount_total || 0), 0)
+      .toFixed(2);
 
-      potentialIncome = +totalPendingSessions
-        .reduce((total, session) => total + (session.amount_total || 0), 0)
-        .toFixed(2)
+    potentialIncome = +totalPendingSessions
+      .reduce((total, session) => total + (session.amount_total || 0), 0)
+      .toFixed(2);
 
-      closingRate = +(
-        (totalClosedSessions.length / checkoutSessions.data.length) * 100
-      ).toFixed(2)
+    closingRate = +(
+      (totalClosedSessions.length / checkoutSessions.data.length) *
+      100
+    ).toFixed(2);
   }
 
-  return <div className="relative h-full">
-     {!agencyDetails.connectAccountId && (
+  return (
+    <div className="relative h-full">
+      {!agencyDetails.connectAccountId && (
         <div className="absolute -top-10 -left-10 right-0 bottom-0 z-30 flex items-center justify-center backdrop-blur-md bg-background/50">
           <Card>
             <CardHeader>
@@ -115,7 +124,7 @@ const Page = async ({
               </small>
             </CardHeader>
             <CardContent className="text-sm text-muted-foreground">
-            Total revenue generated as reflected in your stripe dashboard.
+              Total revenue generated as reflected in your stripe dashboard.
             </CardContent>
             <DollarSign className="absolute right-4 top-4 text-muted-foreground" />
           </Card>
@@ -174,8 +183,36 @@ const Page = async ({
             <Goal className="absolute right-4 top-4 text-muted-foreground" />
           </Card>
         </div>
+        <div className="flex gap-4 xl:!flex-row flex-col">
+          <Card className="p-4 flex-1">
+            <CardHeader>
+              <CardTitle>Transaction History</CardTitle>
+            </CardHeader>
+            <AreaChart
+              className="text-sm stroke-primary"
+              data={[
+                ...(totalClosedSessions || []),
+                ...(totalPendingSessions || []),
+              ]}
+              index="created"
+              categories={["amount_total"]}
+              colors={["primary"]}
+              yAxisWidth={30}
+              showAnimation={true}
+            />
+          </Card>
+          <Card className="xl:w-[400px] w-full">
+            <CardHeader>
+              <CardTitle>Conversions</CardTitle>
+            </CardHeader>
+            <CardContent>
+              
+            </CardContent>
+          </Card>
+        </div>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Page;
