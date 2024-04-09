d947b9b44e88baff6313786ceade4571677c39a1
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-09 12:04:25 +0100

    define variabel needed for calculate

--- src/components/global/pipeline-value.tsx
+++ src/components/global/pipeline-value.tsx
@@ -1,9 +1,16 @@
 'use client'
-import React from 'react'
+import { getPipelines } from '@/lib/queries'
+import { Prisma } from '@prisma/client'
+import React, { useState } from 'react'
 
-type Props = {}
+type Props = {
+    subaccountId: string
+}
 
-const PipelineValue = (props: Props) => {
+const PipelineValue = ({subaccountId}: Props) => {
+    const [pipelines, setPipelines] = useState<Prisma.PromiseReturnType<typeof getPipelines>>([])
+    const [selectedPipelineId, setSelectedPipelineId] = useState('')
+    const [pipelineClosedValue, setPipeineClosedValue] = useState(0)
   return (
     <div>PipelineValue</div>
   )
