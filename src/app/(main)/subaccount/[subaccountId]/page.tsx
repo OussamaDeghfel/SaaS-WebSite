b2bb713b5ac65a3dee97ced368b9ebcc4dc34d58
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-09 11:44:21 +0100

    get funnels

--- src/app/(main)/subaccount/[subaccountId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -1,10 +1,92 @@
+import BlurPage from '@/components/global/blur-page'
+import { db } from '@/lib/db'
+import { stripe } from '@/lib/stripe'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  params: {subaccountId: string}
+  searchParams: {
+    code: string
+  }
+}
 
-const SubAccountMainPage = (props: Props) => {
+const SubAccountMainPage = async ({params, searchParams}: Props) => {
+  let currency = 'USD'
+  let sessions
+  let totalClosedSessions
+  let totalPendingSessions
+  let net = 0
+  let potentialIncome = 0
+  let closingRate = 0
+  const currentYear = new Date().getFullYear()
+  const startDate = new Date(`${currentYear}-01-01T00:00:00Z`).getTime() / 1000
+  const endDate = new Date(`${currentYear}-12-31T23:59:59Z`).getTime() / 1000
+
+
+  const subaccountDetails = await db.subAccount.findUnique({
+    where: {
+      id: params.subaccountId
+    }
+  })
+
+  if(!subaccountDetails) return
+
+  if(subaccountDetails.connectAccountId) {
+    const response = await stripe.accounts.retrieve({
+      stripeAccount: subaccountDetails.connectAccountId
+    })
+    currency = response.default_currency?.toUpperCase() || 'USD'
+
+    const checkoutSessions = await stripe.checkout.sessions.list(
+      {created: {gte: startDate, lte: endDate}, limit: 100},
+      {
+        stripeAccount: subaccountDetails.connectAccountId
+      }
+    )
+    sessions = checkoutSessions.data.map((session) => ({
+      ...session,
+      created: new Date(session.created).toLocaleDateString(),
+      amount_total: session.amount_total ? session.amount_total / 100 : 0
+    }))
+
+    totalClosedSessions = checkoutSessions.data
+    .filter((session) => session.status === "complete")
+    .map((session) => ({
+      ...session,
+      created: new Date(session.created).toLocaleDateString(),
+      amount_total: session.amount_total ? session.amount_total / 100 : 0
+    }))
+    totalPendingSessions = checkoutSessions.data
+    .filter((session) => session.status === "open" || session.status === "expired")
+    .map((session) => ({
+      ...session,
+      created: new Date(session.created).toLocaleDateString(),
+      amount_total: session.amount_total ? session.amount_total / 100 : 0
+    }))
+
+    net = +totalClosedSessions
+    .reduce((total, session) => total + (session.amount_total || 0), 0)
+    .toFixed(2)
+    potentialIncome = +totalPendingSessions
+      .reduce((total, session) => total + (session.amount_total || 0), 0)
+      .toFixed(2)
+
+    closingRate = +(
+      (totalClosedSessions.length / checkoutSessions.data.length) *
+      100
+    ).toFixed(2)
+
+    const funnels = await db.funnel.findMany({
+      where: {
+        subAccountId: params.subaccountId
+      },
+      include: {
+        FunnelPages:true
+      }
+    })
+  }
   return (
-    <div>SubAccountPage</div>
+    <BlurPage>hello</BlurPage>
   )
 }
 
