7614154c0f670700ddd89e5269a66395b0b67a33
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-09 11:57:24 +0100

    client rendering component

---
+++ src/components/global/pipeline-value.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+type Props = {}
+
+const PipelineValue = (props: Props) => {
+  return (
+    <div>PipelineValue</div>
+  )
+}
+
+export default PipelineValue
