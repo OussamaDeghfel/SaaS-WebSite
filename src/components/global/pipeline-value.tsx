eddcf409b38ea1cccd0155ea9bea23c974c3c48c
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-09 12:06:34 +0100

    fetching data from subaccount

--- src/components/global/pipeline-value.tsx
+++ src/components/global/pipeline-value.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { getPipelines } from '@/lib/queries'
 import { Prisma } from '@prisma/client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
     subaccountId: string
@@ -11,6 +11,16 @@ const PipelineValue = ({subaccountId}: Props) => {
     const [pipelines, setPipelines] = useState<Prisma.PromiseReturnType<typeof getPipelines>>([])
     const [selectedPipelineId, setSelectedPipelineId] = useState('')
     const [pipelineClosedValue, setPipeineClosedValue] = useState(0)
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const res = await getPipelines(subaccountId)
+            setPipelines(res)
+            setSelectedPipelineId(res[0]?.id)
+        }
+        fetchData()
+    },[subaccountId])
+    
   return (
     <div>PipelineValue</div>
   )
