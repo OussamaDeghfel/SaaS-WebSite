6a9076359bb1211a2153a1f99c37ef7de0d9a785
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-09 13:58:48 +0100

    show closed and total Value

--- src/components/global/pipeline-value.tsx
+++ src/components/global/pipeline-value.tsx
@@ -1,52 +1,76 @@
-'use client'
-import { getPipelines } from '@/lib/queries'
-import { Prisma } from '@prisma/client'
-import React, { useEffect, useMemo, useState } from 'react'
+"use client";
+import { getPipelines } from "@/lib/queries";
+import { Prisma } from "@prisma/client";
+import React, { useEffect, useMemo, useState } from "react";
+import { Card, CardDescription, CardHeader } from "../ui/card";
 
 type Props = {
-    subaccountId: string
-}
+  subaccountId: string;
+};
 
-const PipelineValue = ({subaccountId}: Props) => {
-    const [pipelines, setPipelines] = useState<Prisma.PromiseReturnType<typeof getPipelines>>([])
-    const [selectedPipelineId, setSelectedPipelineId] = useState('')
-    const [pipelineClosedValue, setPipelineClosedValue] = useState(0)
+const PipelineValue = ({ subaccountId }: Props) => {
+  const [pipelines, setPipelines] = useState<
+    Prisma.PromiseReturnType<typeof getPipelines>
+  >([]);
+  const [selectedPipelineId, setSelectedPipelineId] = useState("");
+  const [pipelineClosedValue, setPipelineClosedValue] = useState(0);
 
-    useEffect(()=>{
-        const fetchData = async () => {
-            const res = await getPipelines(subaccountId)
-            setPipelines(res)
-            setSelectedPipelineId(res[0]?.id)
-        }
-        fetchData()
-    },[subaccountId])
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getPipelines(subaccountId);
+      setPipelines(res);
+      setSelectedPipelineId(res[0]?.id);
+    };
+    fetchData();
+  }, [subaccountId]);
 
-    const totalPipelineValue = useMemo(() => {
-        if(pipelines.length){
-            return (
-                pipelines.find((pipeline) => pipeline.id === selectedPipelineId)
-                ?.Lane?.reduce((totalLanes, lane, currentLaneIndex, array) => {
-                    const laneTicketsTotal = lane.Tickets.reduce(
-                        (totalTickets, ticket) => totalTickets + Number(ticket?.value),0
-                    )
-                    if(currentLaneIndex === array.length-1) {
-                        setPipelineClosedValue(laneTicketsTotal || 0)
-                        return totalLanes
-                    }
-                    return totalLanes + laneTicketsTotal
-                },0) || 0
-            )
-        }
-        return 0
-    },[selectedPipelineId, pipelines])
+  const totalPipelineValue = useMemo(() => {
+    if (pipelines.length) {
+      return (
+        pipelines
+          .find((pipeline) => pipeline.id === selectedPipelineId)
+          ?.Lane?.reduce((totalLanes, lane, currentLaneIndex, array) => {
+            const laneTicketsTotal = lane.Tickets.reduce(
+              (totalTickets, ticket) => totalTickets + Number(ticket?.value),
+              0
+            );
+            if (currentLaneIndex === array.length - 1) {
+              setPipelineClosedValue(laneTicketsTotal || 0);
+              return totalLanes;
+            }
+            return totalLanes + laneTicketsTotal;
+          }, 0) || 0
+      );
+    }
+    return 0;
+  }, [selectedPipelineId, pipelines]);
 
-    const pipelineRate = useMemo(() => {
-        (pipelineClosedValue / (totalPipelineValue + pipelineClosedValue)) * 100
-    },[totalPipelineValue, pipelineClosedValue])
+  const pipelineRate = useMemo(() => {
+    (pipelineClosedValue / (totalPipelineValue + pipelineClosedValue)) * 100;
+  }, [totalPipelineValue, pipelineClosedValue]);
 
   return (
-    <div>PipelineValue</div>
-  )
-}
+    <Card className="relative w-full xl:w-[350px]">
+      <CardHeader>
+        <CardDescription>PipeLine Value</CardDescription>
+        <small className="text-xs text-muted-foreground">
+          PipeLine Progress
+        </small>
+        <div className="flex items-center justify-between">
+          <div>
+            <p className="text-xs text-muted-foreground">
+              Closed ${pipelineClosedValue}
+            </p>
+          </div>
+          <div>
+            <p className="text-xs text-muted-foreground">
+              Total ${totalPipelineValue + pipelineClosedValue}
+            </p>
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+};
 
-export default PipelineValue
+export default PipelineValue;
