580ac7432e55f0c0d1ae015c8bdd8bf359499928
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-16 12:03:02 +0100

    define variables and useEffect for customModal window for upgrade the subscription

--- src/app/(main)/agency/[agencyId]/billing/_component/subscription-helper.tsx
+++ src/app/(main)/agency/[agencyId]/billing/_component/subscription-helper.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import SubscriptionFormWrapper from '@/components/forms/subscription-form/subscription-form-wrapper'
+import CustomModal from '@/components/global/custom-modal'
+import { PricesList } from '@/lib/types'
+import { useModal } from '@/providers/modal-provider'
+import { useSearchParams } from 'next/navigation'
+import React, { useEffect } from 'react'
 
-type Props = {}
+type Props = {
+    prices: PricesList['data']
+    customerId: string
+    planExists: boolean
+}
 
 const SubscriptionHelper = (props: Props) => {
+    const {setOpen} = useModal()
+    const searchParams = useSearchParams()
+    const plan = searchParams.get('plan')
+
+    useEffect(() => {
+        if(plan)
+            setOpen(
+                <CustomModal
+                    title='Upgrade Plan!'
+                    subheading='Get started today to get access to premium features'
+                >
+                    <SubscriptionFormWrapper
+                        planExists={props.planExists}
+                        customerId={props.customerId}
+                    />
+                </CustomModal>,
+                async () => ({
+                    plans: {
+                        defaultPriceId: plan ? plan : '',
+                        plans: props.prices,
+                    }
+                })
+            )
+    },[plan])
   return (
     <div>SubscriptionHelper</div>
   )
