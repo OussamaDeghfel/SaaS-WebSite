03c4b910fca30dd360cca4047a8ba856961f0b41
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-04-16 12:23:14 +0100

    client redering

--- src/app/(main)/agency/[agencyId]/billing/_component/subscription-helper.tsx
+++ src/app/(main)/agency/[agencyId]/billing/_component/subscription-helper.tsx
@@ -1,3 +1,4 @@
+'use client'
 import SubscriptionFormWrapper from '@/components/forms/subscription-form/subscription-form-wrapper'
 import CustomModal from '@/components/global/custom-modal'
 import { PricesList } from '@/lib/types'
